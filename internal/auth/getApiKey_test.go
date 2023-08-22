add32955983628d8dadf85ebbc4f3e626d0672b6
Puran Singh <purandixit@gmail.com>
2023-08-22 21:54:51 +0530

    broken code test

---
+++ internal/auth/getApiKey_test.go
@@ -0,0 +1,33 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetApiKey(t *testing.T) {
+	cases := []struct {
+		input    http.Header
+		expected string
+	}{
+		{
+			input:    map[string][]string{"Authorization": {"ApiKey aljfalakjfaakffksfajafka"}},
+			expected: "aljfalakjfaakffksfajafka",
+		},
+		{
+			input:    map[string][]string{"Authorization": {"ApiKey aaljfalakjfaakffksfajafka"}},
+			expected: "aljfalakjfaakffksfajafka",
+		},
+	}
+	for _, cs := range cases {
+		actual, err := GetAPIKey(cs.input)
+		if err != nil {
+			t.Errorf("%v is malformed input", cs.input)
+		}
+		expectedString := cs.expected
+		if actual != expectedString {
+			t.Errorf("%v is not equal to %v", cs.input, cs.expected)
+		}
+	}
+
+}
