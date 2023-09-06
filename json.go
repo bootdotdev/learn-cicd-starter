ff43808f0478f0675a49a16ce544ee8da0c3d4a3
EdisNezo <edis.nezovski@outlook.de>
2023-09-06 18:24:22 +0200

    error fix

--- json.go
+++ json.go
@@ -27,5 +27,8 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	_, err = w.Write(dat)
+	if err != nil {
+		log.Printf("Critical error writing response: %s", err)
+	}
 }
