36f7e7ad42b45e36d020f16ee94e5fa9abf32ac7
Emil Malmsten <emil_malmsten@hotmail.com>
2023-09-08 18:37:11 +0700

    add test and update ci

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,44 @@
+package auth
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	type test struct {
+		apiKey string
+		expectedResult string
+	}
+	
+	tests := []test{
+		{
+			apiKey: "key",
+			expectedResult: "key",
+		},
+		{
+			apiKey: "123",
+			expectedResult: "1234",
+		},
+		{
+			apiKey: "nlkwaejlkawe",
+			expectedResult: "nlkwaejlkawe",
+		},
+	}
+
+	for _, tc := range tests {
+		headers := make(http.Header)
+		headers.Set("Authorization", "ApiKey " + tc.apiKey)
+	
+		got, err := GetAPIKey(headers)
+		if err != nil {
+			fmt.Println(err)
+			t.Fatal("test error")
+		}
+	
+		if (tc.expectedResult != got) {
+			t.Fatalf("got %s but wanted %s", got, tc.apiKey)
+		}	
+	}
+}
