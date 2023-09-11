b8cb1b65d16335606af795360aaf06ab2ab7af77
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2023-09-11 18:38:26 +0100

    add workflows

--- json.go
+++ json.go
@@ -20,12 +20,11 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
-	dat, err := json.Marshal(payload)
+	w.WriteHeader(code)
+	err := json.NewEncoder(w).Encode(payload)
 	if err != nil {
 		log.Printf("Error marshalling JSON: %s", err)
 		w.WriteHeader(500)
 		return
 	}
-	w.WriteHeader(code)
-	w.Write(dat)
 }
