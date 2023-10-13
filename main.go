adf44648d872e329e1892792075e91ddad0cf538
Charles Ozochukwu <charlesozochukwu2004@gmail.com>
2023-10-13 23:55:26 +0100

    updated main.go

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		log.Fatal("PORT environment variable is not set")
+		log.Fatal("PORT environment variable is not already set")
 	}
 
 	apiCfg := apiConfig{}
