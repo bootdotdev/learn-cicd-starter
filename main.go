51fe78fc4c942134ce445e75304169b88ada4f71
heysourin <sourin07@gmail.com>
2023-10-25 12:23:09 +0530

    test branch created

--- main.go
+++ main.go
@@ -9,13 +9,10 @@ import (
 	"net/url"
 	"os"
 	"strings"
-
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
-
 	"github.com/bootdotdev/learn-cicd-starter/internal/database"
-
 	_ "github.com/go-sql-driver/mysql"
 )
 
