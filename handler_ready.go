b2b87abe106f1ae9cbd87153567c3bf80ea85ec2
scuzzy <scuzzy@pupfilthy.com>
2023-11-08 19:34:22 -0800

    Add handlerReadiness documentation

--- handler_ready.go
+++ handler_ready.go
@@ -2,6 +2,8 @@ package main
 
 import "net/http"
 
+// handlerReadiness returns a JSON status. Use it to ensure the server is ready
+// for incoming requests.
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	respondWithJSON(w, http.StatusOK, map[string]string{"status": "ok"})
 }
