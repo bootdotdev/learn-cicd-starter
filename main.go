e7d34dff2ed400d8b26cecd7fc120f1ae42d0426
razvan-soare <soare.razvan94@gmail.com>
2023-11-27 21:55:38 +0000

    Break text formatting

--- main.go
+++ main.go
@@ -26,10 +26,13 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
-func main(){
+func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
+
+	
+	
 	}
 
 	port := os.Getenv("PORT")
