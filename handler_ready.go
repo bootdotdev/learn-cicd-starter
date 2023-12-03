6d5d61dd5c5c69adabdb090cf91559857226bfe4
zoumas <zoumas@protonmail.com>
2023-12-03 21:00:16 +0200

    Ignore the request parameter on the readiness handler

--- handler_ready.go
+++ handler_ready.go
@@ -2,6 +2,6 @@ package main
 
 import "net/http"
 
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+func handlerReadiness(w http.ResponseWriter, _ *http.Request) {
 	respondWithJSON(w, http.StatusOK, map[string]string{"status": "ok"})
 }
