02fe59df806efd2502267a7078d036b7ba3cf8fb
Bayan2019 <saparbayevabt@gmail.com>
2023-12-19 13:11:30 -0800

    failed test

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -26,7 +26,7 @@ func TestGetAPIKey(t *testing.T) {
 		{
 			input:           http.Header{"Authorization": {"ApiKey"}},
 			expected_string: "",
-			expected_error:  errors.New("malformed authorization header"),
+			expected_error:  errors.New("malformed authorization hheader"),
 		},
 		{
 			input:           http.Header{"Authorization": {"ApiKey Key"}},
