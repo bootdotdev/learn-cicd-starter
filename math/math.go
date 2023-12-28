1aa93fffc8f41397e5d9b586aef3efcb4b57afd6
craig-johnson0128 <craig_johnson1987@hotmail.com>
2023-12-28 14:54:35 -0500

    added math unit test

---
+++ math/math.go
@@ -0,0 +1,11 @@
+package math
+
+// Add is our function that sums two integers
+func Add(x, y int) (res int) {
+	return x + y
+}
+
+// Subtract subtracts two integers
+func Subtract(x, y int) (res int) {
+	return x - y
+}
