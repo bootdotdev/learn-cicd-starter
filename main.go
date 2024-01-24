2c0ad62eb5f2fc096d7d988878a6ee1b42b5f28c
dhdorr <dhdorr@gmail.com>
2024-01-23 20:04:22 -0800

    testing ci unit tests2

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
 	}
 
-	port := os.Getenv("PORTs")
+	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT environment variable is not set")
 	}
