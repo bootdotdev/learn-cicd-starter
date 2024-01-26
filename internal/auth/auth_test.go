ddef27304b38a6569d699542db93da1ed7f0356b
John Taylor <johnmichaeltaylor79@gmail.com>
2024-01-26 09:54:57 +0000

    Api auth header tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,58 @@
+package auth_test
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/bootdotdev/learn-cicd-starter/internal/auth"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name  string
+		input http.Header
+		want  string
+		err   error
+	}{
+		{
+			name:  "no header",
+			input: http.Header{},
+			want:  "",
+			err:   auth.ErrNoAuthHeaderIncluded,
+		},
+		{
+			name: "empty auth header ",
+			input: http.Header{
+				"Authorization": []string{""},
+			},
+			want: "",
+			err:  auth.ErrNoAuthHeaderIncluded,
+		},
+		{
+			name: "unknown auth type",
+			input: http.Header{
+				"Authorization": []string{"Bearer token"},
+			},
+			want: "",
+			err:  auth.ErrMalformedAuthHeader,
+		},
+		{
+			name: "ok",
+			input: http.Header{
+				"Authorization": []string{"ApiKey token"},
+			},
+			want: "token",
+			err:  nil,
+		},
+	}
+	for _, tc := range tests {
+		got, err := auth.GetAPIKey(tc.input)
+		if got != tc.want {
+			t.Errorf("got %v, wanted: %v", got, tc.want)
+		}
+		if tc.err != nil && !errors.Is(err, tc.err) {
+			t.Errorf("got error: %v, wanted error: %v", tc.err, err)
+		}
+	}
+}
