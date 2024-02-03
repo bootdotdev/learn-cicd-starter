a5119e6819346f1f9b7cd8159facc0a0a134e13d
Jerry Zhang <jerryzhang.dev@gmail.com>
2024-02-03 15:35:41 -0500

    fix broken GetApiKey fn

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -16,7 +16,7 @@ func GetAPIKey(headers http.Header) (string, error) {
 		return "", ErrNoAuthHeaderIncluded
 	}
 	splitAuth := strings.Split(authHeader, " ")
-	if len(splitAuth) < 2 || splitAuth[0] != "ApiKe" {
+	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
 		return "", ErrMalformedAuthHeader
 	}
 
