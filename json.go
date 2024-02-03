f6adf37a733737b8d372eb66e2065e56904e7cfa
Jerry Zhang <jerryzhang.dev@gmail.com>
2024-02-03 16:33:37 -0500

    fix gosec issues

--- json.go
+++ json.go
@@ -26,6 +26,12 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		w.WriteHeader(500)
 		return
 	}
+	_, err = w.Write(dat)
+	if err != nil {
+		log.Printf("Error writing response: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
 	w.WriteHeader(code)
-	w.Write(dat)
 }
