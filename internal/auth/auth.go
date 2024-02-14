4983abb96549d7278119ba555a9e22ec7c7d5e7c
Norvin Vilchez <n.vilch01@gmail.com>
2024-02-14 13:34:23 -0500

    testing with broken code

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -11,7 +11,7 @@ var ErrMalformedAuthHeader = errors.New("malformed authorization header")
 
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
-	authHeader := headers.Get("Authorization")
+	authHeader := headers.Get("Authorizatio")
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
