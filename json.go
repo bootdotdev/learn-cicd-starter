e732a0af285dfe8ac5ff9c6d7d435170c70dba9a
Hoa Pham <hoa.phamlethai@gmail.com>
2024-02-19 07:21:52 -0800

    fix gosec errors

--- json.go
+++ json.go
@@ -27,5 +27,10 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	_, err = w.Write(dat)
+	if err != nil {
+		// Handle the error, for example:
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
