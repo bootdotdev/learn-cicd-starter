ba9bd6fd650118ed3db18333be69b575f79cbe75
Oliviero Taleghani <o.taleghani@talesign.com>
2024-03-26 16:39:44 +0100

    new tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,40 @@
+package auth
+
+import (
+	"testing"
+	"net/http"
+)
+
+func TestBearer(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	apiKeyExpected := "sandro"
+	req.Header.Set("Authorization", "Bearer "+apiKeyExpected)
+
+	apiKey, err := GetAPIKey(req.Header)
+	if err == nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+	if apiKey == apiKeyExpected {
+		t.Error("This should not return the request apikey")
+	}
+}
+
+func TestCorrect(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	apiKeyExpected := "sandro"
+	req.Header.Set("Authorization", "ApiKey "+apiKeyExpected)
+
+	apiKey, err := GetAPIKey(req.Header)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+	if apiKey != apiKeyExpected {
+		t.Errorf("Expected no error, got %v", err)
+	}
+}
