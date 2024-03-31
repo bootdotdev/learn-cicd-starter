53c10d7d898f3e203c52df64f2dedc52d63dc13b
Uthpala Heenatigala <uthpala@passionatepeople.io>
2024-03-31 18:06:11 +0200

    added ci formatting check

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,11 +1,11 @@
 package auth
 
 import (
-  "testing"
+	"testing"
 )
 
 func TestGetAPIKey(t *testing.T) {
-  if ( 2 < 1 ) {
-    t.Error("2 cannot be larger than 1")
-  }
+	if 2 < 1 {
+		t.Error("2 cannot be larger than 1")
+	}
 }
