9ed23625967a04b0469041d7962838968dff35cf
Inviber <bkelley4600@gmail.com>
2024-04-08 19:06:15 -0500

    Added staticcheck for linting for ci

--- main.go
+++ main.go
@@ -24,6 +24,12 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
+func unused() {
+    // this function does nothing
+    // and is called nowhere
+}
+
+
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
