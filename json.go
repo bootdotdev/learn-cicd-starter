43d48e9ef2ba89f2b0c95d543a859bf2e729fd18
githubt@tokyuez.net <github@tokyuez.net>
2024-04-27 21:57:56 +0200

    fixed security

--- json.go
+++ json.go
@@ -19,13 +19,17 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 }
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	dat, err := json.Marshal(payload)
-	if err != nil {
-		log.Printf("Error marshalling JSON: %s", err)
-		w.WriteHeader(500)
-		return
-	}
-	w.WriteHeader(code)
-	w.Write(dat)
+    w.Header().Set("Content-Type", "application/json")
+    dat, err := json.Marshal(payload)
+    if err != nil {
+        log.Printf("Error marshalling JSON: %s", err)
+        w.WriteHeader(500)
+        return
+    }
+
+    w.WriteHeader(code)
+    _, err = w.Write(dat)
+    if err != nil {
+        log.Printf("Error writing response: %s", err)
+    }
 }
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -89,8 +90,11 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:         ":" + port,
+		Handler:      router,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
