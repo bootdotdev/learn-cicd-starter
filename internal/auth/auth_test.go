3798e27b24514835b0ae080186e51b01c9c6f1c2
githubt@tokyuez.net <github@tokyuez.net>
2024-04-27 21:31:50 +0200

    push

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -6,34 +6,34 @@ import (
 )
 
 func TestGetAPIKey(t *testing.T) {
-    headers := http.Header{}
-    headers.Add("Authorization", "ApiKey testkey")
+	headers := http.Header{}
+	headers.Add("Authorization", "ApiKey testkey")
 
-    apiKey, err := GetAPIKey(headers)
-    if err != nil {
-        t.Errorf("Unexpected error: %v", err)
-    }
+	apiKey, err := GetAPIKey(headers)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
 
-    if apiKey != "testkey" {
-        t.Errorf("Expected 'testkey', got '%s'", apiKey)
-    }
+	if apiKey != "testkey" {
+		t.Errorf("Expected 'testkey', got '%s'", apiKey)
+	}
 }
 
 func TestGetAPIKeyNoAuthHeader(t *testing.T) {
-    headers := http.Header{}
+	headers := http.Header{}
 
-    _, err := GetAPIKey(headers)
-    if err != ErrNoAuthHeaderIncluded {
-        t.Errorf("Expected 'no authorization header included' error, got '%v'", err)
-    }
+	_, err := GetAPIKey(headers)
+	if err != ErrNoAuthHeaderIncluded {
+		t.Errorf("Expected 'no authorization header included' error, got '%v'", err)
+	}
 }
 
 func TestGetAPIKeyMalformedAuthHeader(t *testing.T) {
-    headers := http.Header{}
-    headers.Add("Authorization", "malformedheader")
+	headers := http.Header{}
+	headers.Add("Authorization", "malformedheader")
 
-    _, err := GetAPIKey(headers)
-    if err == nil || err.Error() != "malformed authorization header" {
-        t.Errorf("Expected 'malformed authorization header' error, got '%v'", err)
-    }
+	_, err := GetAPIKey(headers)
+	if err == nil || err.Error() != "malformed authorization header" {
+		t.Errorf("Expected 'malformed authorization header' error, got '%v'", err)
+	}
 }
