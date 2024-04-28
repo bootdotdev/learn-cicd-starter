d50cafa0b823633c2c327d6310e216d9b957e362
Gergana Naydenova <gtnaydenova@gmail.com>
2024-04-28 07:24:04 +0300

    feat: add tests to workflows, check failing

--- go.mod
+++ go.mod
@@ -7,13 +7,17 @@ require (
 	github.com/go-chi/cors v1.2.1
 	github.com/google/uuid v1.3.0
 	github.com/joho/godotenv v1.5.1
+	github.com/stretchr/testify v1.9.0
 	github.com/tursodatabase/libsql-client-go v0.0.0-20240220085343-4ae0eb9d0898
 )
 
 require (
 	github.com/antlr/antlr4/runtime/Go/antlr/v4 v4.0.0-20230512164433-5d1fd1a340c9 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/klauspost/compress v1.15.15 // indirect
 	github.com/libsql/sqlite-antlr4-parser v0.0.0-20230802215326-5cb5bb604475 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	nhooyr.io/websocket v1.8.7 // indirect
 )
---
+++ internal/auth/auth_test.go
@@ -0,0 +1,43 @@
+package auth
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetAuth(t *testing.T) {
+	t.Run("get api key success", func(t *testing.T) {
+		apiKey := "test-api-key"
+		headers := http.Header{
+			"Authorization": {fmt.Sprintf("ApiKey %s", apiKey)},
+		}
+
+		result, err := GetAPIKey(headers)
+
+		assert.NoError(t, err)
+		assert.Equal(t, apiKey, result)
+	})
+
+	t.Run("get api key no header error", func(t *testing.T) {
+		result, err := GetAPIKey(nil)
+
+		assert.Equal(t, err, ErrNoAuthHeaderIncluded)
+		assert.Equal(t, "", result)
+	})
+
+	t.Run("get api key malformed header error", func(t *testing.T) {
+		headers := http.Header{
+			"Authorization": {"WrongApiKey api-key"},
+		}
+
+		result, err := GetAPIKey(headers)
+
+		assert.NoError(t, err)
+		assert.EqualError(t, err, "malformed authorization header")
+		assert.Equal(t, "", result)
+	})
+
+}
