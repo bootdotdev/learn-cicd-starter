c6e9a53885f7c6a515edd3964151f0cd273fc790
jpforbes5151 <jpforbes5151@gmail.com>
2024-04-28 13:59:26 -0400

    checking cd.yml validity

--- main.go
+++ main.go
@@ -91,7 +91,7 @@ func main() {
 	srv := &http.Server{
 		Addr:        ":" + port,
 		Handler:     router,
-		ReadTimeout: 300,
+		ReadTimeout: 3000,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
