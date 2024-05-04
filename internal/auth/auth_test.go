cf777dc940541693bda959a1b9e50aaa3adafe25
mohammadshabab <shababkhan04@gmail.com>
2024-05-04 11:20:31 +0530

    fixing test cases

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"errors"
 	"net/http"
 	"testing"
 )
@@ -12,18 +13,25 @@ func Test_GetAPIKey(t *testing.T) {
 		name   string
 		input  map[string][]string
 		output string
-		errr   string
+		errr   error
 	}{
 		{name: "valid Header", input: map[string][]string{"Authorization": {"ApiKey Bearer"}}, output: "Bearer", errr: nil},
-		{name: "valid Header", input: map[string][]string{"Authorization": {"ApiKey"}}, output: "", errr: "malformed authorization header"},
-		{name: "valid Header", input: map[string][]string{}, output: "", errr: "no authorization header "},
+		{name: "valid Header", input: map[string][]string{"Authorization": {"ApiKey"}}, output: "", errr: errors.New("malformed authorization header")},
+		{name: "valid Header", input: map[string][]string{}, output: "", errr: errors.New("no authorization header included")},
 	}
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			got, err := GetAPIKey(tc.input)
-			if tc.output != got && err.Error() != tc.errr {
-				t.Fatalf("%s outputExpected: %v, outputGot: %v, errorExpected: %v, errorGot: ", tc.name, tc.output, tc.errr, err)
+			if err != nil {
+				if err.Error() != tc.errr.Error() {
+					t.Fatalf("%s: errorExpected: %v, errorGot: %v", tc.name, tc.errr, err)
+
+				}
 			}
+			if tc.output != got {
+				t.Fatalf("%s: outputExpected: %v, outputGot: %v", tc.name, tc.output, got)
+			}
+
 		})
 
 	}
