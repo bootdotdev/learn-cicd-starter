6b83e7cbad3b72eedde9e8f8e47fe006496412aa
kavyaMVG <kavya.mvg@gmail.com>
2024-05-11 16:35:25 +0530

    Add style job

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -6,12 +6,12 @@ import (
 	"testing"
 )
 
-func TestAuth(t *testing.T){
-    req, err := http.NewRequest("GET", "http://localhost:8080", nil) 
-    req.Header.Set("Authorization", "ApiKey apiKey")
-    got, error := GetAPIKey(req.Header)
-    want := "apiKey"
-    if !reflect.DeepEqual(want, got) || err != nil || error != nil {
-        t.Fatalf("expected: %v, got: %v", want, got)
-    }
+func TestAuth(t *testing.T) {
+	req, err := http.NewRequest("GET", "http://localhost:8080", nil)
+	req.Header.Set("Authorization", "ApiKey apiKey")
+	got, error := GetAPIKey(req.Header)
+	want := "apiKey"
+	if !reflect.DeepEqual(want, got) || err != nil || error != nil {
+		t.Fatalf("expected: %v, got: %v", want, got)
+	}
 }
