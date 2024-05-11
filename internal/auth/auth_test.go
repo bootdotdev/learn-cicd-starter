ddc88b381cf7aac2d2888980fde5b1dbc0d72bdf
kavyaMVG <kavya.mvg@gmail.com>
2024-05-11 15:52:44 +0530

    update ci.yml

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,17 @@
+package auth
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestAuth(t *testing.T){
+    req, err := http.NewRequest("GET", "http://localhost:8080", nil) 
+    req.Header.Set("Authorization", "ApiKey apiKey")
+    got, error := GetAPIKey(req.Header)
+    want := "apiKey"
+    if !reflect.DeepEqual(want, got) || err != nil || error != nil {
+        t.Fatalf("expected: %v, got: %v", want, got)
+    }
+}
