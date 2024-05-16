0b294458da25f72b15b7a1c7d771d915cfe9b76c
Pavel Perner <pavel.perner@gem.cz>
2024-05-16 16:23:12 +0200

    Add formatting

--- handler_ready_test.go
+++ handler_ready_test.go
@@ -8,16 +8,15 @@ import (
 )
 
 func TestHandleNotesGet(t *testing.T) {
-  req, _ := http.NewRequest("GET", "/healthz", nil)
-  rr := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/healthz", nil)
+	rr := httptest.NewRecorder()
 
-  handlerReadiness(rr, req)
+	handlerReadiness(rr, req)
 
-  var got map[string]string
-  json.Unmarshal(rr.Body.Bytes(), &got)
+	var got map[string]string
+	json.Unmarshal(rr.Body.Bytes(), &got)
 
-  if got["status"] != "ok" {
-    t.Errorf("Expected the 'status' key of the response to be set to 'ok'. Got '%s'", got["error"])
-  }
+	if got["status"] != "ok" {
+		t.Errorf("Expected the 'status' key of the response to be set to 'ok'. Got '%s'", got["error"])
+	}
 }
-
--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -9,22 +9,22 @@ import (
 )
 
 func TestGetAPIKey(t *testing.T) {
-  type test struct {
-    input http.Header
-    want string
-    err error
-  }
+	type test struct {
+		input http.Header
+		want  string
+		err   error
+	}
 
-  tests := []test{
-    { input: http.Header{ "Authorization": { "ApiKey 1234" }}, want: "1234", err: nil },
-    { input: http.Header{ "Authorization": { "1234" }}, want: "", err: errors.New("malformed authorization header") },
-    { input: http.Header{}, want: "", err: ErrNoAuthHeaderIncluded },
-  }
+	tests := []test{
+		{input: http.Header{"Authorization": {"ApiKey 1234"}}, want: "1234", err: nil},
+		{input: http.Header{"Authorization": {"1234"}}, want: "", err: errors.New("malformed authorization header")},
+		{input: http.Header{}, want: "", err: ErrNoAuthHeaderIncluded},
+	}
 
-  for _, tc := range tests {
-    got, err := GetAPIKey(tc.input)
-    
-    assert.Equal(t, tc.want, got)
-    assert.Equal(t, tc.err, err)
-  }
+	for _, tc := range tests {
+		got, err := GetAPIKey(tc.input)
+
+		assert.Equal(t, tc.want, got)
+		assert.Equal(t, tc.err, err)
+	}
 }
