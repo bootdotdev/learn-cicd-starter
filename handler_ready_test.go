34c5e018b8d57ef894304fb3da809824a22f239f
Pavel Perner <pavel.perner@gem.cz>
2024-05-16 13:57:46 +0200

    Add tests

---
+++ handler_ready_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandleNotesGet(t *testing.T) {
+  req, _ := http.NewRequest("GET", "/healthz", nil)
+  rr := httptest.NewRecorder()
+
+  handlerReadiness(rr, req)
+
+  var got map[string]string
+  json.Unmarshal(rr.Body.Bytes(), &got)
+
+  if got["status"] != "ok" {
+    t.Errorf("Expected the 'status' key of the response to be set to 'ok'. Got '%s'", got["error"])
+  }
+}
+
