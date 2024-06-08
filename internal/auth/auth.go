43de4ec20f6028ddb0b53e202db955edb9f287db
greap <greapsoww7@gmail.com>
2024-06-07 22:01:39 -0700

    breaking auth.go for workflow test in ci.yml to see if the tests catch the failure/the workflow failure occurs on GitHub

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -14,7 +14,7 @@ func GetAPIKey(headers http.Header) (string, error) {
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
-	splitAuth := strings.Split(authHeader, " ")
+	plitAuth := strings.Split(authHeader, " ")
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
 		return "", errors.New("malformed authorization header")
 	}
