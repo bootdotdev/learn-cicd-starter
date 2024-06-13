d718367e3940da33caf4904727c29b1878859cea
SpacePudding <michael.vu9966@gmail.com>
2024-06-13 12:22:09 +0200

    Fix unitTest error

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -9,7 +9,7 @@ import (
 func TestAuth(t *testing.T) {
 	bearerToken := "ey.123FADolo1233"
 	header := http.Header{}
-	header.Set("Authorization", "Bearer "+bearerToken)
+	header.Set("Authorization", "ApiKey "+bearerToken)
 	got, _ := GetAPIKey(header)
 	want := bearerToken
 	if !reflect.DeepEqual(want, got) {
