70153c730df3cbfface56e36cead98a81fe09f23
SpacePudding <michael.vu9966@gmail.com>
2024-06-13 12:20:43 +0200

    Add unitTest

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,18 @@
+package auth
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestAuth(t *testing.T) {
+	bearerToken := "ey.123FADolo1233"
+	header := http.Header{}
+	header.Set("Authorization", "Bearer "+bearerToken)
+	got, _ := GetAPIKey(header)
+	want := bearerToken
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("expected: %v, got: %v", want, got)
+	}
+}
