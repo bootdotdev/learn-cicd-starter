a42675502a97786bcdebfe2269318d1083d122c9
grodier <grodier3@gmail.com>
2024-06-22 22:09:45 -0400

    Add failing test and ci to run tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,22 @@
+package auth
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	t.Run("returns api key", func(t *testing.T) {
+		want := "Test_Key"
+
+		headers := http.Header{}
+		headers.Add("Authorization", fmt.Sprintf("ApiKey %s", want))
+
+		got, _ := GetAPIKey(headers)
+
+		if got == want {
+			t.Errorf("wanted apiKey %s, got %s", want, got)
+		}
+	})
+}
