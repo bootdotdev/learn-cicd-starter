68e518113179212a96847c28f57f94d97941d5f2
Maxwell Kopitz <accounts@maxwell.dev>
2024-07-01 17:37:51 -0400

    trigger style

--- main.go
+++ main.go
@@ -82,7 +82,7 @@ func main() {
 		v1Router.Post("/users", apiCfg.handlerUsersCreate)
 		v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerUsersGet))
 		v1Router.Get("/notes", apiCfg.middlewareAuth(apiCfg.handlerNotesGet))
-		v1Router.Post("/notes", apiCfg.middlewareAuth(apiCfg.handlerNotesCreate))
+		    v1Router.Post("/notes", apiCfg.middlewareAuth(apiCfg.handlerNotesCreate))
 	}
 
 	v1Router.Get("/healthz", handlerReadiness)
