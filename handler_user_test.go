72a6dcb2645464ce25714de1fc95d4f5cc86d222
ronkiker <ron.s.kiker@gmail.com>
2024-07-07 13:27:33 -0600

    fix test

--- handler_user_test.go
+++ handler_user_test.go
@@ -7,7 +7,7 @@ func TestGenerateRandomSHA(t *testing.T) {
 	if err != nil {
 		t.Errorf("Unexpected error generating SHA")
 	}
-	if len(sha) != 4 {
+	if len(sha) != 64 {
 		t.Errorf("Expected 64 bytes, got %d", len(sha))
 	}
 }
