c865809e564e6b5c4f8f80c4d1fcb0c633cd45e5
Tanner Shimanek <tannershimanek@gmail.com>
2024-07-08 10:51:58 -0600

    updated unit test to a happypath test

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -5,55 +5,24 @@ import (
 	"testing"
 )
 
-func TestGetAPIKey(t *testing.T) {
-	tests := []struct {
-		name          string
-		headers       http.Header
-		expectedKey   string
-		expectedError error
-	}{
-		{
-			name:          "No Authorization Header",
-			headers:       http.Header{},
-			expectedKey:   "",
-			expectedError: ErrNoAuthHeaderIncluded,
-		},
-		{
-			name: "Malformed Authorization Header - No ApiKey Prefix",
-			headers: http.Header{
-				"Authorization": []string{"Bearer token123"},
-			},
-			expectedKey:   "",
-			expectedError: errors.New("malformed authorization header"),
-		},
-		{
-			name: "Malformed Authorization Header - No Key",
-			headers: http.Header{
-				"Authorization": []string{"ApiKey"},
-			},
-			expectedKey:   "",
-			expectedError: errors.New("malformed authorization header"),
-		},
-		{
-			name: "Valid Authorization Header",
-			headers: http.Header{
-				"Authorization": []string{"ApiKey key123"},
-			},
-			expectedKey:   "key123",
-			expectedError: nil,
-		},
+func TestGetAPIKeyHappyPath(t *testing.T) {
+	headers := http.Header{
+		"Authorization": []string{"ApiKey key123"},
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			apiKey, err := GetAPIKey(tt.headers)
-			if apiKey != tt.expectedKey {
-				t.Errorf("expected key %s, got %s", tt.expectedKey, apiKey)
-			}
-			if (err != nil && tt.expectedError == nil) || (err == nil && tt.expectedError != nil) || (err != nil && err.Error() != tt.expectedError.Error()) {
-				t.Errorf("expected error %v, got %v", tt.expectedError, err)
-			}
-		})
+	expectedKey := "key123"
+	expectedError := error(nil)
+
+	apiKey, err := GetAPIKey(headers)
+
+	// Check API Key
+	if apiKey != expectedKey {
+		t.Errorf("expected key %s, got %s", expectedKey, apiKey)
+	}
+
+	// Check error
+	if err != expectedError {
+		t.Errorf("expected no error, got %v", err)
 	}
 }
 
