526b6b867501f814225ee54001698b43759c48ca
fthe <lcslianandfthe.cs@gmail.com>
2024-07-19 01:21:21 +0900

    fixed: fixed test want incorretc

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -14,15 +14,15 @@ func TestAuthAPI(t *testing.T) {
 	}{
 		"AuthorizationSucessful": {header: "Authorization", input: "ApiKey key-input", want: "key-input"},
 		"AuthorizationFailed":    {header: "WrongHeader", input: "ApiKey key-input", want: ""},
-		"ApiKeyNoSpaceFailed":    {header: "WrongHeader", input: "ApiKeykey-input", want: "f"},
-		"ApiKeyWrongKeyFailed":   {header: "WrongHeader", input: "ApIKey key-input", want: "f"},
+		"ApiKeyNoSpaceFailed":    {header: "WrongHeader", input: "ApiKeykey-input", want: ""},
+		"ApiKeyWrongKeyFailed":   {header: "WrongHeader", input: "ApIKey key-input", want: ""},
 	}
 	for name, tc := range tests {
 		testHeader := http.Header{}
 		testHeader.Set(tc.header, tc.input)
 		key, err := GetAPIKey(testHeader)
 		if !reflect.DeepEqual(key, tc.want) {
-			t.Fatalf("%s: expect: %v, got: %v", name, tc.want, err.Error())
+			t.Fatalf("%s: expect: '%v', got: '%v'\nerror: %v", name, tc.want, key, err.Error())
 		}
 	}
 
