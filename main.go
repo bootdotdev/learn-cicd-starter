ced5b696099ec7d964555df2955a16f0c920df27
bhashimoto <brunothashimoto@gmail.com>
2024-07-22 13:03:53 -0300

    fixed formatting

--- main.go
+++ main.go
@@ -90,9 +90,9 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
-		ReadHeaderTimeout: time.Second*10,
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: time.Second * 10,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
