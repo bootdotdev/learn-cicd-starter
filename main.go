278a814d6aa7b345daaf20b992613038bd7a5eba
ahgr3y <leehh97@gmail.com>
2024-07-24 11:44:34 +0800

    Added staticcheck

--- main.go
+++ main.go
@@ -24,6 +24,10 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
+func unused() {
+	// Nothing to see here, shoo!
+}
+
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
