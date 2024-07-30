b9c1b5304cdf1faabb124b466134b7e4fa61a61b
Diogo Carvalho <diogosouzacarvalho@gmail.com>
2024-07-30 13:55:21 +0200

    fix: security warnings

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -91,6 +92,7 @@ func main() {
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: router,
+		ReadTimeout: time.Second * 1,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
