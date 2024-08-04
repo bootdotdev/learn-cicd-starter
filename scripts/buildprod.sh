5ddcb620cd4163d8642ddaf44073a6dc5af001e4
eymardfreiredev <eymardfreire@gmail.com>
2024-08-04 18:42:39 -0400

    Fix build script to point to the correct directory

--- scripts/buildprod.sh
+++ scripts/buildprod.sh
@@ -4,5 +4,11 @@ set -e
 # Navigate to the root of the project directory
 cd "$(dirname "$0")/.."
 
+# Print the current directory
+pwd
+
+# List the contents of the current directory
+ls -la
+
 # Build the Go project
 go build -o notely ./notely
