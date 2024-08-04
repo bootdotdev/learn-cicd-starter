cb9068e55405bb807cf3dfd1dfa2ec33a04575e1
eymardfreiredev <eymardfreire@gmail.com>
2024-08-04 18:54:10 -0400

    Ensure buildprod.sh has execute permissions

--- scripts/buildprod.sh
+++ scripts/buildprod.sh
@@ -4,11 +4,5 @@ set -e
 # Navigate to the root of the project directory
 cd "$(dirname "$0")/.."
 
-# Print the current directory
-pwd
-
-# List the contents of the current directory
-ls -la
-
 # Build the Go project
-go build -o notely ./notely
+go build -o notely
