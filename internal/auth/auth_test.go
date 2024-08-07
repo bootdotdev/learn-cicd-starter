ce07b7c795cc4319cb22123664d190e9a5341f83
hf-chow <honfungchow@gmail.com>
2024-08-07 09:33:54 +0800

    test new job

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,41 +1,41 @@
 package auth
 
 import (
-    "testing"
-    "net/http"
+	"net/http"
+	"testing"
 )
 
 func TestGetAPIKey(t *testing.T) {
-    headers := make(http.Header)
+	headers := make(http.Header)
 
-    // Case 1: No auth header included
-    apiKey, err := GetAPIKey(headers)
-    if err != ErrNoAuthHeaderIncluded {
-        t.Errorf("Expected error: %v, but got: %v", ErrNoAuthHeaderIncluded, err)
-    }
-    if apiKey != "" {
-        t.Errorf("Expected empty API key, but got %s", apiKey)
-    }
+	// Case 1: No auth header included
+	apiKey, err := GetAPIKey(headers)
+	if err != ErrNoAuthHeaderIncluded {
+		t.Errorf("Expected error: %v, but got: %v", ErrNoAuthHeaderIncluded, err)
+	}
+	if apiKey != "" {
+		t.Errorf("Expected empty API key, but got %s", apiKey)
+	}
 
-    // Case 2: Malformed auth header
-    headers.Set("Authorization", "Bearer token")
-    apiKey, err = GetAPIKey(headers)
-    expectedErr := "malformed authorization header"
-    if err.Error() != expectedErr {
-        t.Errorf("Expected error: %s, but got: %v", expectedErr, err)
-    }
-    if apiKey != "" {
-        t.Errorf("Expected empty API key, but got: %s", apiKey)
-    }
+	// Case 2: Malformed auth header
+	headers.Set("Authorization", "Bearer token")
+	apiKey, err = GetAPIKey(headers)
+	expectedErr := "malformed authorization header"
+	if err.Error() != expectedErr {
+		t.Errorf("Expected error: %s, but got: %v", expectedErr, err)
+	}
+	if apiKey != "" {
+		t.Errorf("Expected empty API key, but got: %s", apiKey)
+	}
 
-    // Case 3: Valid auth header
-    headers.Set("Authorization", "ApiKey my-api-key")
-    apiKey, err = GetAPIKey(headers)
-    if err != nil {
-        t.Errorf("Expected no error, but got :%v", err)
-    }
-    expectedAPIKey := "my-api-key"
-    if apiKey != expectedAPIKey {
-        t.Errorf("Expected API key: %s, but got: %s", expectedAPIKey, apiKey)
-    }
+	// Case 3: Valid auth header
+	headers.Set("Authorization", "ApiKey my-api-key")
+	apiKey, err = GetAPIKey(headers)
+	if err != nil {
+		t.Errorf("Expected no error, but got :%v", err)
+	}
+	expectedAPIKey := "my-api-key"
+	if apiKey != expectedAPIKey {
+		t.Errorf("Expected API key: %s, but got: %s", expectedAPIKey, apiKey)
+	}
 }
