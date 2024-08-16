a96f090edd0d2e1fafd0805e1ec73ba14c24b316
Abdulmumin Akinde <lanreywaju97@gmail.com>
2024-08-16 20:05:54 +0100

    Add unit test for GetApiKey

---
+++ get_api_key_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/bootdotdev/learn-cicd-starter/internal/auth"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	type test struct {
+		input map[string][]string
+		want  string
+	}
+	var header = make(map[string][]string)
+	header["Authorization"] = []string{"ApiKey randomAccessKey"}
+	header["Content-Type"] = []string{"application/json"}
+
+	tests := []test{
+		{input: header, want: "RandomAccessKey"},
+	}
+
+	for _, test := range tests {
+		got, _ := auth.GetAPIKey(test.input)
+		if !reflect.DeepEqual(test.want, got) {
+			t.Fatalf("Expected %#v, got %#v", test.want, got)
+		}
+
+	}
+}
