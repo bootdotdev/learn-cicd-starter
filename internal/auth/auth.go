07b05e15c26fbbf6ecdf1baac411d365ad698ac4
caselhos <caselhosgoncalo@gmail.com>
2024-08-21 05:46:02 +0100

    test3

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
+var ErrNoAuthHeaderIncluded = errors.New("authorization header not included")
 
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
