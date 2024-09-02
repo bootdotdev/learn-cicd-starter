e146780ec8d9d430c45a0b4157e84f3ded6d0e08
Antony Gandonou migan <gmigan.a@gmail.com>
2024-09-02 12:45:08 +0200

    added a temporary test error to test ci workflow

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -16,3 +16,29 @@ func TestGetAPIKey(t *testing.T) {
 		t.Fatalf("expected: %v, got: %v", want, got)
 	}
 }
+
+func TestGetAPIKeyEmpty(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	got, err := GetAPIKey(req.Header)
+	if err != nil || got != "" {
+		t.Fatalf("expected: a error and empty value, got: %v", got)
+	}
+}
+
+func TestGetAPIKeyMalformed_1(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	req.Header.Set("Authorization", "ApiKey")
+	got, err := GetAPIKey(req.Header)
+	if err == nil || got != "" {
+		t.Fatalf("expected: a error and empty value, got: %v", got)
+	}
+}
+
+func TestGetAPIKeyMalformed_2(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	req.Header.Set("Authorization", "Malformation secret")
+	got, err := GetAPIKey(req.Header)
+	if err == nil || got != "" {
+		t.Fatalf("expected: a error and empty value, got: %v", got)
+	}
+}
