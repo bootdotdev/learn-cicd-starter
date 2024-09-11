acaabfa3c92f1771b9aa8d088eace83ca2ffaba2
thomasem <me@thomasem.net>
2024-09-11 09:45:50 -0500

    format current code before CI formatting

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,18 +1,18 @@
 package auth
 
 import (
-    "testing"
-    "net/http"
+	"net/http"
+	"testing"
 )
 
 func TestGetAPIKey(t *testing.T) {
-    testHeaders := http.Header{}
-    testHeaders.Add("Authorization", "ApiKey 123456")
-    result, err := GetAPIKey(testHeaders)
-    if err != nil {
-        t.Errorf("failed getting API key: %s", err)
-    }
-    if result != "123456" {
-        t.Errorf("want 123456, got %s", result)
-    }
+	testHeaders := http.Header{}
+	testHeaders.Add("Authorization", "ApiKey 123456")
+	result, err := GetAPIKey(testHeaders)
+	if err != nil {
+		t.Errorf("failed getting API key: %s", err)
+	}
+	if result != "123456" {
+		t.Errorf("want 123456, got %s", result)
+	}
 }
