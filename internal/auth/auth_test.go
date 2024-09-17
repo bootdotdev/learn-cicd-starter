c778d2c4a5a3de9f2d918803a295ec6097f61f78
Sam Gabel <sam@gabelcloud.com>
2024-09-17 09:54:35 -0700

    update auth test
    Signed-off-by: Sam Gabel <sam@gabelcloud.com>

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -2,28 +2,60 @@ package auth
 
 import (
 	"net/http"
+	"strings"
 	"testing"
 )
 
 func TestGetAPIKey(t *testing.T) {
-	headerMap := map[string][]string{
-		"Authorization": {"ApiKey abc123"},
-	}
-
-	input := []struct {
-		header      http.Header
-		expectedKey string
+	tests := map[string]struct {
+		key       string
+		value     string
+		expect    string
+		expectErr string
 	}{
-		{
-			header:      headerMap,
-			expectedKey: "abc123",
+		"no auth header": {
+			expectErr: "no authorization header",
+		},
+		"no auth header value": {
+			key:       "Authorization",
+			expectErr: "no authorization header",
+		},
+		"malformed auth header value v1": {
+			key:       "Authorization",
+			value:     "-",
+			expectErr: "malformed authorization header",
+		},
+		"malformed auth header value v2": {
+			key:       "Authorization",
+			value:     "Bearer xxxxxx",
+			expectErr: "malformed authorization header",
+		},
+		"correctly formed auth header": {
+			key:       "Authorization",
+			value:     "ApiKey xxxxxx",
+			expect:    "xxxxxx",
+			expectErr: "not expecting an error",
 		},
 	}
 
-	for _, tt := range input {
-		apiKey, err := GetAPIKey(tt.header)
-		if apiKey != tt.expectedKey {
-			t.Fatalf("the actual apiKey=%s does not match the expected=%s\nError:%v", apiKey, tt.expectedKey, err)
-		}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			header := http.Header{}
+			header.Add(test.key, test.value)
+
+			output, err := GetAPIKey(header)
+			if err != nil {
+				if strings.Contains(err.Error(), test.expectErr) {
+					return
+				}
+				t.Errorf("Unexpected: TestGetAPIKey:%v\n", err)
+				return
+			}
+
+			if output != test.expect {
+				t.Errorf("Unexpected: TestGetAPIKey:%s", output)
+				return
+			}
+		})
 	}
 }
