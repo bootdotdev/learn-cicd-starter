61becb404bde609541e55353a279658492948ddb
Triyaambak S <triyaambaks@Triyaambaks-MacBook.local>
2024-10-02 16:50:23 +0530

    added go sec for security integration and fixed one security issue in json.go

--- json.go
+++ json.go
@@ -27,5 +27,8 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	_, err = w.Write(dat)
+	if err != nil {
+		log.Printf("Error writing JSON: %s", err)
+	}
 }
