62e5ea7654dc0819162fb0202ce39b99e03f13c3
Mateusz Salach <mateuszsalach00@gmail.com>
2024-10-04 20:20:11 +0200

    Fix fmt tests

--- main.go
+++ main.go
@@ -90,8 +90,8 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
 		ReadHeaderTimeout: 10 * time.Second,
 	}
 
