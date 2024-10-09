08bf2a2a9ab3670f04fd245e7e84e1dde50a4c9c
kevinarchambeau <xxerexx@gmail.com>
2024-10-09 16:12:46 -0700

    added some unit checks

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,42 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKeyNoAuth(t *testing.T) {
+	headers := http.Header{}
+	headers.Add("Content-Type", "application/json")
+	result, err := GetAPIKey(headers)
+	if result != "" {
+		t.Fatalf("Unexpected return: %s", result)
+	}
+	if err == nil {
+		t.Fatal("No error for headers without Authorization")
+	}
+}
+
+func TestGetAPIKeyInvalidAuth(t *testing.T) {
+	headers := http.Header{}
+	headers.Add("Authorization", "")
+	result, err := GetAPIKey(headers)
+	if result != "" {
+		t.Fatalf("Unexpected return: %s", result)
+	}
+	if err == nil {
+		t.Fatal("No error for headers without Authorization")
+	}
+}
+
+func TestGetAPIKeyValidAuth(t *testing.T) {
+	headers := http.Header{}
+	headers.Add("Authorization", "ApiKey 213234asdas4")
+	result, err := GetAPIKey(headers)
+	if result == "" {
+		t.Fatalf("Unexpected return: %s", result)
+	}
+	if err != nil {
+		t.Fatalf("Unexpected error for valid auth header format: %s", err)
+	}
+}
