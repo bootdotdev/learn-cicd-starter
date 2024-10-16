a497342c63b145ba33b28ded251b5154c339ffd4
a3ternum <sena.ozdemir.1@gmail.com>
2024-10-16 11:56:34 +0200

    changed workflow

--- main.go
+++ main.go
@@ -16,7 +16,10 @@ import (
 
 	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
-
+func unused() {
+    // this function does nothing
+    // and is called nowhere
+}
 type apiConfig struct {
 	DB *database.Queries
 }
