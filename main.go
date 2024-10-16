cd00ae568207183c4979e3dfde3e59bfa33b32d9
a3ternum <sena.ozdemir.1@gmail.com>
2024-10-16 13:33:52 +0200

    fixed main.go

--- main.go
+++ main.go
@@ -91,6 +91,7 @@ func main() {
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: router,
+		ReadHeaderTimeout: 10 * time.Second, // Set to an appropriate duration
 	}
 
 	log.Printf("Serving on port: %s\n", port)
