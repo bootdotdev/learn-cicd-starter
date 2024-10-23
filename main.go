f6507950c06d54b1ab67c1b4c91b1a9b6980a3ff
jsandberg07 <jsandberg07@gmail.com>
2024-10-23 15:15:01 -0400

    minor spelling mistake

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ type apiConfig struct{ DB *database.Queries }
 //go:embed static/*
 var staticFiles embed.FS
 
-func main() {
+func main(){
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
