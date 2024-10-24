6c4effafa8eed87b774d16d641519b5dbc4d35ef
relios898 <adventureguy898@gmail.com>
2024-10-24 18:01:23 -0400

    fixed issue with timeout

--- main.go
+++ main.go
@@ -92,7 +92,7 @@ func main() {
 	srv := &http.Server{
 		Addr:              ":" + port,
 		Handler:           router,
-		ReadHeaderTimeout: time.Duration(120),
+        ReadHeaderTimeout: time.Duration(1) * time.Minute,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
