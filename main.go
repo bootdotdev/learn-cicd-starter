fe06def51f884bf7728aeafcd2760017206f8b4b
jsandberg07 <148825692+jsandberg07@users.noreply.github.com>
2024-10-25 14:03:04 -0400

    Addtests (#6)
    * beans'd it
    
    * double beans
    
    * added ci tests
    
    * removed exit from tests
    
    * bad tests lmao
    
    * fixed tests
    
    * coverage added to tests
    
    * added badge to readme
    
    * double beans (#2)
    
    * double beans
    
    * added ci tests
    
    * removed exit from tests
    
    * bad tests lmao
    
    * fixed tests
    
    * coverage added to tests
    
    * added badge to readme
    
    * added formatting tests
    
    * minor spelling mistake
    
    * trying to get stuff to run
    
    * double beans
    
    * work please
    
    * added static check
    
    * removed unused function
    
    * more changes :^3
    
    * The time andrew came in
    
    * The time andrew left
    
    * CD setup
    
    * Addtests (#4)
    
    * double beans
    
    * added ci tests
    
    * removed exit from tests
    
    * bad tests lmao
    
    * fixed tests
    
    * coverage added to tests
    
    * added badge to readme
    
    * added formatting tests
    
    * minor spelling mistake
    
    * trying to get stuff to run
    
    * double beans
    
    * work please
    
    * added static check
    
    * removed unused function
    
    * more changes :^3
    
    * The time andrew came in
    
    * The time andrew left
    
    * CD setup
    
    * yummy rumbly

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -17,9 +18,10 @@ import (
 	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
-type apiConfig struct {
-	DB *database.Queries
-}
+type apiConfig struct{ DB *database.Queries }
+
+// i bean'd it
+// double beans
 
 //go:embed static/*
 var staticFiles embed.FS
@@ -89,8 +91,9 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: time.Second * 10,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
