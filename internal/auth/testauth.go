e3555cc705b9055849fe7fa5391b96d2353091fd
Mateusz <m.z.kazmierczak@gmail.com>
2024-10-28 17:40:15 +0100

    fmt

--- internal/auth/testauth.go
+++ internal/auth/testauth.go
@@ -1,14 +1,14 @@
 package auth
 
-import(
-  "reflect"
-  "testing"
+import (
+	"reflect"
+	"testing"
 )
 
-func TestGetAPIKey(t *testing.T){
-  want:=[]string{"a"}
-  got:=[]string{"a"}
-  if !reflect.DeepEqual(want, got){
-    t.Fatalf("fatal")
-  }
+func TestGetAPIKey(t *testing.T) {
+	want := []string{"a"}
+	got := []string{"a"}
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("fatal")
+	}
 }
