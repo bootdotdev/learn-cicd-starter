6dba71cd83a50fd47cd63ca7a8990e873982ac81
jsandberg07 <jsandberg07@gmail.com>
2024-10-29 09:48:35 -0400

    another one

--- handler_user.go
+++ handler_user.go
@@ -31,6 +31,8 @@ func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
+	// meow
+
 	err = cfg.DB.CreateUser(r.Context(), database.CreateUserParams{
 		ID:        uuid.New().String(),
 		CreatedAt: time.Now().UTC().Format(time.RFC3339),
