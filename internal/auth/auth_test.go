22e5c9d84af673ea1c0b98fedff11234879941b7
marbh56 <nathan.a.sanders2@gmail.com>
2024-10-30 12:40:42 -0400

    auth_test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,61 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name          string
+		headers       http.Header
+		expectedKey   string
+		expectedError error
+	}{
+		{
+			name:          "No Authorization Header",
+			headers:       http.Header{},
+			expectedKey:   "A", // Temp Broken
+			expectedError: ErrNoAuthHeaderIncluded,
+		},
+		{
+			name: "Malformed Authorization Header (No ApiKey Prefix)",
+			headers: http.Header{
+				"Authorization": []string{"Bearer abcdef12345"},
+			},
+			expectedKey:   "",
+			expectedError: errors.New("malformed authorization header"),
+		},
+		{
+			name: "Malformed Authorization Header (Missing Token)",
+			headers: http.Header{
+				"Authorization": []string{"ApiKey"},
+			},
+			expectedKey:   "",
+			expectedError: errors.New("malformed authorization header"),
+		},
+		{
+			name: "Valid Authorization Header",
+			headers: http.Header{
+				"Authorization": []string{"ApiKey abcdef12345"},
+			},
+			expectedKey:   "abcdef12345",
+			expectedError: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			key, err := GetAPIKey(tt.headers)
+
+			if key != tt.expectedKey {
+				t.Errorf("expected key %v, got %v", tt.expectedKey, key)
+			}
+
+			if (err != nil && tt.expectedError == nil) || (err == nil && tt.expectedError != nil) || (err != nil && err.Error() != tt.expectedError.Error()) {
+				t.Errorf("expected error %v, got %v", tt.expectedError, err)
+			}
+		})
+	}
+}
