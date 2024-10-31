104f6df2b5f5002284986ce05173282f9b95a2ad
joseds <josediazseng@gmx.de>
2024-10-31 21:00:47 +0100

    added tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,35 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey_FailsWithoutApiKeyEntry(t *testing.T) {
+	headers := make(http.Header)
+	headers.Set("Authorization", "Bearer 1234")
+	_, err := GetAPIKey(headers)
+	if err != ErrMalformedAuthHeader {
+		t.Errorf("Expected error %v, got %v", ErrMalformedAuthHeader, err)
+	}
+}
+
+func TestGetAPIKey_FailsWithoutAuthHeader(t *testing.T) {
+	headers := make(http.Header)
+	_, err := GetAPIKey(headers)
+	if err != ErrNoAuthHeaderIncluded {
+		t.Errorf("Expected error %v, got %v", ErrNoAuthHeaderIncluded, err)
+	}
+}
+
+func TestGetAPIKey_Success(t *testing.T) {
+	headers := make(http.Header)
+	headers.Set("Authorization", "ApiKey 1234")
+	key, err := GetAPIKey(headers)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+	if key != "1234" {
+		t.Errorf("Expected key to be 1234, got %v", key)
+	}
+}
