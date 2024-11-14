838b8f49a60ae9b2e3ab3ff9a454a5ad3b7813ab
mrkiz-git <mark.elman@gmail.com>
2024-11-14 10:13:08 +0200

    gosec fixed

--- json.go
+++ json.go
@@ -27,5 +27,8 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+
+	if _, err := w.Write(dat); err != nil {
+		log.Printf("Failed to write response: %v", err)
+	}
 }
