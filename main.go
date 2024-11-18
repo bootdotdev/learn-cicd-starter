f47557b8cc0382b55bbacb690b0e054637046158
ScooballyD <carverlee@live.com>
2024-11-18 15:37:14 -0600

    fixed version

--- main.go
+++ main.go
@@ -25,9 +25,6 @@ type apiConfig struct {
 var staticFiles embed.FS
 
 func main() {
-	func unusued() {
-		// does nothing
-	}
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
