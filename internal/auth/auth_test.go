87f16b517bd3a32efd3d6d1146d00d2907467f17
George Kourtzaridis <w84it@hackthebox.eu>
2024-11-23 15:40:20 +0200

    correct tests

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -16,36 +16,37 @@ func TestGetAPIKey(t *testing.T) {
 		want  want
 	}
 
-	tests := []test{
-		{
+	tests := map[string]test{
+		"Test empty api key": {
 			input: http.Header{"Authorization": []string{""}},
 			want:  want{str: "", err: "no authorization header included"},
 		},
-		{
+		"Test malformed api key": {
 			input: http.Header{"Authorization": []string{"Bearer fakeapikey"}},
 			want:  want{str: "", err: "malformed authorization header"},
 		},
-		{
+		"Test correct api key": {
 			input: http.Header{"Authorization": []string{"ApiKey 1234123123"}},
 			want:  want{str: "1234123123", err: ""},
 		},
 	}
 
-	for _, tc := range tests {
-		got, err := GetAPIKey(tc.input)
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := GetAPIKey(tc.input)
 
-		if got != tc.want.str {
-			t.Errorf("got API key: %q, want: %q", got, tc.want.str)
-		}
-
-		// Check the error message
-		if err != nil {
-			if err.Error() != tc.want.err {
-				t.Errorf("got error: %q, want: %q", err.Error(), tc.want.err)
+			if got != tc.want.str {
+				t.Errorf("got API key: %q, want: %q", got, tc.want.str)
 			}
-		} else if tc.want.err != "" {
-			t.Errorf("expected error: %q, but got none", tc.want.err)
-		}
-	}
 
+			// Check the error message
+			if err != nil {
+				if err.Error() != tc.want.err {
+					t.Errorf("got error: %q, want: %q", err.Error(), tc.want.err)
+				}
+			} else if tc.want.err != "" {
+				t.Errorf("expected error: %q, but got none", tc.want.err)
+			}
+		})
+	}
 }
