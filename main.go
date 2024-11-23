acba38d4997cc3ba862805849b3c90c84e9eab48
DanielJacob1998 <dmariathasan@gmail.com>
2024-11-23 16:33:29 -0500

    update

--- main.go
+++ main.go
@@ -24,18 +24,18 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
-func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
+func main(){
+	err:=godotenv.Load(".env")
+	if err!=nil{
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
 	}
 
-	port := os.Getenv("PORT")
-	if port == "" {
+	port:=os.Getenv("PORT")
+	if port==""{
 		log.Fatal("PORT environment variable is not set")
 	}
 
-	apiCfg := apiConfig{}
+	apiCfg:=apiConfig{}
 
 	// https://github.com/libsql/libsql-client-go/#open-a-connection-to-sqld
 	// libsql://[your-database].turso.io?authToken=[your-auth-token]
