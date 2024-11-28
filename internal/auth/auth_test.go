5b364eb185c08f74c453a76b3fd70c04b4639f86
Obegarde <5763603+Obegarde@users.noreply.github.com>
2024-11-28 13:23:13 +0100

    ANoth

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,41 +1,39 @@
 package auth
 
 import (
+	"errors"
+	"net/http"
 	"reflect"
 	"testing"
-	"net/http"
-	"errors"
-
 )
 
-func TestGetAPIKey(t *testing.T){
+func TestGetAPIKey(t *testing.T) {
 	testHeader := http.Header{}
-	testHeader.Set("Authorization","ApiKey 123456")
+	testHeader.Set("Authorization", "ApiKey 123456")
 
-	got,_ := GetAPIKey(testHeader)
+	got, _ := GetAPIKey(testHeader)
 	want := "123456"
-	if !reflect.DeepEqual(want,got){
+	if !reflect.DeepEqual(want, got) {
 		t.Fatalf("expected: %v, got: %v", want, got)
 	}
 
-
 }
 
-func TestGetApiKeyNoAuthHeader(t *testing.T){
+func TestGetApiKeyNoAuthHeader(t *testing.T) {
 	testHeader := http.Header{}
-	_,err := GetAPIKey(testHeader)
+	_, err := GetAPIKey(testHeader)
 	want := ErrNoAuthHeaderIncluded
-	if !errors.Is(want,err){
+	if !errors.Is(want, err) {
 		t.Fatalf("expected: %v, got: %v", want, err)
 	}
 }
 
-func TestGetApiKeyBadAuth(t *testing.T){
+func TestGetApiKeyBadAuth(t *testing.T) {
 	testHeader := http.Header{}
-	testHeader.Set("Authorization","123456")
-	_,err := GetAPIKey(testHeader)
+	testHeader.Set("Authorization", "123456")
+	_, err := GetAPIKey(testHeader)
 	want := "malformed authorization header"
-	if err.Error() != want{
+	if err.Error() != want {
 		t.Fatalf("expected: %v, got: %v", want, err)
 	}
 }
