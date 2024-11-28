084bfe6c01d80bf1b2d6aac681ba13cdbd13d4b6
Obegarde <5763603+Obegarde@users.noreply.github.com>
2024-11-28 17:39:54 +0100

    Fix gosec issues

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
-
+	"time"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
@@ -91,6 +91,7 @@ func main() {
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: router,
+		ReadHeaderTimeout: 15 * time.Second,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
