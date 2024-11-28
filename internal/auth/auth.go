0ad7c6ff7e314a10353f1d1e119501a0540b9851
srinivassivaratri <srinivassivaratri1122@gmail.com>
2024-11-27 21:24:28 -0600

    Fix fcode formatting

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -2,24 +2,21 @@ package auth
 
 import (
 	"errors"
-	"fmt"
 	"net/http"
 	"strings"
 )
 
 var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
-var ErrMalformedAuthHeader = errors.New("malformed authorization header")
 
-// GetAPIKey extracts API key from Authorization header
+// GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
-		return "", fmt.Errorf("auth error: %w", ErrNoAuthHeaderIncluded)
+		return "", ErrNoAuthHeaderIncluded
 	}
-	
 	splitAuth := strings.Split(authHeader, " ")
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
-		return "", fmt.Errorf("auth error: %w", ErrMalformedAuthHeader)
+		return "", errors.New("malformed authorization header")
 	}
 
 	return splitAuth[1], nil
