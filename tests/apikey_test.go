fe87ffbc8fdd3d9009c84c653263636e106e8407
apunco <puncochar.a@gmail.com>
2024-12-01 21:22:49 +0100

    added test

---
+++ tests/apikey_test.go
@@ -0,0 +1,30 @@
+package tests
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+
+	"github.com/bootdotdev/learn-cicd-starter/internal/auth"
+)
+
+func TestSplit(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []string
+		want  string
+	}{
+		{name: "simple", input: []string{"Authorization", "ApiKey 123456"}, want: "1234566"},
+	}
+
+	for _, tc := range tests {
+		header := http.Header{}
+		header.Set(tc.input[0], tc.input[1])
+		got, err := auth.GetAPIKey(header)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Fatalf("%s: expected: %v, got: %v", tc.name, tc.want, got)
+		} else if err != nil {
+			t.Fatalf("%s: expected: %v, got: %v", tc.name, tc.want, got)
+		}
+	}
+}
