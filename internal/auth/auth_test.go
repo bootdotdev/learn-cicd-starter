f587de0955469eca8ab1436b45a326f8a3ae69b6
katsuikeda <katsuyukiikeda142857@gmail.com>
2024-12-19 19:29:57 -0800

    Test CI properly catches failing tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,68 @@
+package auth
+
+import (
+	"net/http"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	authHeader := http.Header{}
+	authHeader.Set("Authorization", "ApiKey validKey")
+
+	got, err := GetAPIKey(authHeader)
+	want := "validKey"
+	if err != nil && !reflect.DeepEqual(got, want) {
+		t.Fatalf("expected: %v, got: %v, err: %v", want, got, err)
+	}
+
+	tests := map[string]struct {
+		headers     http.Header
+		wantKey     string
+		wantErr     bool
+		errorString string
+	}{
+		"valid authorization header": {
+			headers: http.Header{"Authorization": []string{"ApiKey " + "validKey"}},
+			wantKey: "validKey",
+			wantErr: true,
+		},
+		"missing authorization header": {
+			headers:     http.Header{},
+			wantErr:     true,
+			errorString: "no authorization header included",
+		},
+		"incorrect scheme": {
+			headers:     http.Header{"Authorization": []string{"Bearer " + "validKey"}},
+			wantErr:     true,
+			errorString: "malformed authorization header",
+		},
+		"mutliple keys": {
+			headers: http.Header{"Authorization": []string{"ApiKey key1 key2"}},
+			wantKey: "key1",
+			wantErr: false,
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			gotKey, err := GetAPIKey(tc.headers)
+			if (err != nil) != tc.wantErr {
+				t.Fatalf("error: %v, wantErr: %v", err, tc.wantErr)
+				return
+			}
+			if tc.wantErr {
+				if err == nil {
+					t.Fatalf("expected error but got none")
+				} else if !strings.Contains(err.Error(), tc.errorString) {
+					t.Fatalf("error: %v, expected substring: %v", err, tc.errorString)
+				}
+				return
+			}
+			if gotKey != tc.wantKey {
+				t.Fatalf("got: %v, want: %v", gotKey, tc.wantKey)
+			}
+		})
+	}
+}
