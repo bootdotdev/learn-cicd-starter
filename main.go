1f0753c4aaed29f6ca29cb72691d1af3fb862e9e
alexis <alexis.simonian@outlook.com>
2024-12-22 11:17:46 +0100

    corrected security vulnerabilities

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -91,6 +92,8 @@ func main() {
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: router,
+        ReadTimeout: 30 * time.Second,
+        WriteTimeout: 30 * time.Second,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
