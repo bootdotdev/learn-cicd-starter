6c38e0ce7d6e78d19f57ad2782baa07e2cc49f53
javierpoduje <javier.poduje@gmail.com>
2024-12-25 17:37:34 +0100

    remove unused

--- main.go
+++ main.go
@@ -96,8 +96,3 @@ func main() {
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
-
-func unused() {
-    // this function does nothing
-    // and is called nowhere
-}
