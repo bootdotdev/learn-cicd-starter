094942fdb43d70c8f1c3b41a61dfef3cc8899569
Zeke-MA <gregorios.tzikas@gmail.com>
2024-12-27 13:16:11 -0500

    Added auth_test.go with intentional failuers

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,59 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestAPIKey(t *testing.T) {
+	header1 := http.Header{}
+	header2 := http.Header{}
+	header3 := http.Header{}
+
+	header1.Set("Authorization", "token 123456")
+	header2.Set("Auth", "1234")
+	header3.Set("Authorization", "abc123")
+
+	type testCase struct {
+		name        string
+		header      http.Header
+		expectedErr bool
+		expectedKey string
+	}
+
+	tests := []testCase{
+		{
+			name:        "Valid token",
+			header:      header1,
+			expectedErr: true,
+			expectedKey: "123456",
+		},
+		{
+			name:        "Invalid Header Key",
+			header:      header1,
+			expectedErr: false,
+			expectedKey: "",
+		},
+		{
+			name:        "Invalid value",
+			header:      header1,
+			expectedErr: false,
+			expectedKey: "",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			token, err := GetAPIKey(tc.header)
+
+			if (err != nil) != tc.expectedErr {
+				t.Errorf("unexpected error, got %v, expected error: %v", err, tc.expectedErr)
+			}
+
+			if !tc.expectedErr && token != tc.expectedKey {
+				t.Errorf("unexpected token, got %v, expected %v", token, tc.expectedKey)
+			}
+		})
+	}
+
+}
