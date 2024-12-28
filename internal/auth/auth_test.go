a7b25ad8f6c4c277e60bf585b4cbe6c6861bd764
Nathaniel Rosenbloom <112800403+nathanielclassiqio@users.noreply.github.com>
2024-12-28 19:35:46 +0200

    Add style to CI

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,22 +1,21 @@
 package auth
 
 import (
-  "testing"
-  "net/http"
+	"net/http"
+	"testing"
 )
 
 func TestThing(t *testing.T) {
-  if 1 == 2 {
-    t.Fatalf("Oh no oh me oh my numbers")
-  }
+	if 1 == 2 {
+		t.Fatalf("Oh no oh me oh my numbers")
+	}
 }
 
 func TestEmptyAuthHeader(t *testing.T) {
-  var header http.Header = make(map[string][]string)
-  _, err := GetAPIKey(header)
-  s := err.Error()
-  if s != "no authorization header included" {
-    t.Fatalf("Unexpected error: %v", s)
-  }
+	var header http.Header = make(map[string][]string)
+	_, err := GetAPIKey(header)
+	s := err.Error()
+	if s != "no authorization header included" {
+		t.Fatalf("Unexpected error: %v", s)
+	}
 }
-
