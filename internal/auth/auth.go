1bfa5b4f542f30aef12069de0a5a96f497519b49
Xavier <tim88776@gmail.com>
2025-01-10 22:41:35 +0800

    fix unit test ci

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -10,7 +10,7 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
-	authHeader := headers.Get("Authorizatio")
+	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
