04f524eb4fd093a751e4af961c7e09ec8798cef4
AmaNRwt01 <94544552+AmaNRwt01@users.noreply.github.com>
2025-01-18 13:56:07 +0530

    bug: added intentional bug to test workflows

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -3,21 +3,23 @@ package auth
 import (
 	"errors"
 	"net/http"
-	"strings"
+	// "strings"
 )
 
 var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
-	authHeader := headers.Get("Authorization")
-	if authHeader == "" {
-		return "", ErrNoAuthHeaderIncluded
-	}
-	splitAuth := strings.Split(authHeader, " ")
-	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
-		return "", errors.New("malformed authorization header")
-	}
+	return "", errors.New("intentional failure")
 
-	return splitAuth[1], nil
+	// authHeader := headers.Get("Authorization")
+	// if authHeader == "" {
+	// 	return "", ErrNoAuthHeaderIncluded
+	// }
+	// splitAuth := strings.Split(authHeader, " ")
+	// if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
+	// 	return "", errors.New("malformed authorization header")
+	// }
+
+	// return splitAuth[1], nil
 }
