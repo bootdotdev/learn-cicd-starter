78dcdd1c068597d9e482bc20312e782255f6dcf3
charMlessMan80 <carpediem050180@gmail.com>
2025-01-29 21:06:46 +0100

    Corrected main.go DDoS vulnerability

--- main.go
+++ main.go
@@ -89,8 +89,9 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: 5,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
