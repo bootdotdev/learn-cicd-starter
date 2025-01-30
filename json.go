a2ea5d96b803db9dd88638c1f0adb3301757a4e2
KamilMad <kamilul085@gmail.com>
2025-01-30 23:53:20 +0100

    Fix security issues

--- json.go
+++ json.go
@@ -27,7 +27,7 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	_, err := w.Write(dat)
+	_, err = w.Write(dat)
 	if err != nil {
     	log.Printf("failed to write response: %v", err)
 	}
