59c952d29ed1a4f2e747cd0c25ed888ff33f5032
KamilMad <kamilul085@gmail.com>
2025-01-30 18:39:08 +0100

    Write tests - one of them fails

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,54 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name    string
+		headers http.Header
+		want    string
+		wantErr error
+	}{
+		{
+			name: "Valid API Key",
+			headers: http.Header{
+				"Authorization": []string{"ApiKey my-secret-key"},
+			},
+			want:    "wrong-key",
+			wantErr: nil,
+		},
+		{
+			name:    "Missing Authorization Header",
+			headers: http.Header{},
+			want:    "",
+			wantErr: ErrNoAuthHeaderIncluded,
+		},
+		{
+			name: "Malformed Authorization Header",
+			headers: http.Header{
+				"Authorization": []string{"Bearer my-secret-key"},
+			},
+			want:    "",
+			wantErr: errors.New("malformed authorization header"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetAPIKey(tt.headers)
+
+			if got != tt.want {
+				t.Errorf("GetAPIKey() got = %v, want %v", got, tt.want)
+			}
+
+			if (err != nil && tt.wantErr == nil) || (err == nil && tt.wantErr != nil) || (err != nil && err.Error() != tt.wantErr.Error()) {
+				t.Errorf("GetAPIKey() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
