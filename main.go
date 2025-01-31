9138b70f60e66559d4b1c7fe0f5a4eaa0867f376
Emre Ulusoy <emre5tornaci@gmail.com>
2025-01-30 23:03:28 -0600

    added formatting job

--- main.go
+++ main.go
@@ -11,10 +11,9 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
 
 	"github.com/bootdotdev/learn-cicd-starter/internal/database"
-
-	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 type apiConfig struct {
