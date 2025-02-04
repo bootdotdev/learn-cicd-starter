f366262a3b3c83bde492b5893871ca64e3c772d7
ReenigneCA <david.oldford@gmail.com>
2025-02-04 09:21:39 -0500

    fixed auth

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -16,7 +16,7 @@ func GetAPIKey(headers http.Header) (string, error) {
 	}
 	splitAuth := strings.Split(authHeader, " ")
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
-		return "", errors.New("alformed authorization header")
+		return "", errors.New("malformed authorization header")
 	}
 
 	return splitAuth[1], nil
