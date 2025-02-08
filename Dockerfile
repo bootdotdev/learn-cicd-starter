6cf194fd5fdbcb5aacc13b0e40b7746b9f470f1f
Jonathan Petrone <44399523+JonathanPetrone@users.noreply.github.com>
2025-02-08 13:28:39 +0100

    added steps to docker file

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,6 @@ RUN chmod +x notely
 EXPOSE 8080
 ENV PORT=8080
 
+# Be explicit about listening on all interfaces
+ENV HOST=0.0.0.0
 CMD ["./notely"]
--- main.go
+++ main.go
@@ -26,6 +26,7 @@ type apiConfig struct {
 var staticFiles embed.FS
 
 func main() {
+	log.Println("Starting application...")
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
