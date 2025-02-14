9abada760b1d0716a340106bc3697d63e063858a
Nohl Childers <nohlachilders@github.com>
2025-02-13 16:48:28 -0800

    added tests for auth header fetching

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,27 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func testEmptyHeader(t *testing.T) {
+	header := http.Header{}
+	header.Set("Authorization", "")
+	if _, err := GetAPIKey(header); err == ErrNoAuthHeaderIncluded {
+		t.Error("Expected empty header error, found none")
+	}
+}
+
+func testMalformedHeader(t *testing.T) {
+	header := http.Header{}
+	header.Set("Authorization", "something wrong")
+	if _, err := GetAPIKey(header); err == nil {
+		t.Error("Expected malformed authorization header, got none")
+	}
+
+	header.Set("Authorization", "ApiKey")
+	if _, err := GetAPIKey(header); err == nil {
+		t.Error("Expected malformed authorization header, got none")
+	}
+}
