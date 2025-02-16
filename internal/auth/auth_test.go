111153ef5a93cd618731afc8d579dcd364fcef74
tadoodlez <tadoodlez@gmail.com>
2025-02-17 00:19:29 +0800

    test: temporarily break tests to verify CI

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,57 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+type apiKeyResult struct {
+	ApiKey string
+	err    error
+}
+
+func TestGetAPIKey(t *testing.T) {
+	tests := map[string]struct {
+		input http.Header
+		want  apiKeyResult
+	}{
+		"Simple": {
+			input: func() http.Header {
+				h := http.Header{}
+				h.Add("Authorization", "ApiKey different-test-key")
+				return h
+			}(),
+			want: apiKeyResult{"test-key", nil}},
+		"NoAuthHeader": {
+			input: func() http.Header {
+				h := http.Header{}
+				return h
+			}(),
+			want: apiKeyResult{"", ErrNoAuthHeaderIncluded}},
+		"MalformedAuthHeader": {
+			input: func() http.Header {
+				h := http.Header{}
+				h.Add("Authorization", "Badformay")
+				return h
+			}(),
+			want: apiKeyResult{"", errors.New("malformed authorization header")}},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := GetAPIKey(tc.input)
+			if got != tc.want.ApiKey {
+				t.Errorf("expected API key: %v, got: %v", tc.want.ApiKey, got)
+			}
+			if err == nil && tc.want.err == nil {
+				// both nil, that's good
+			} else if err == nil || tc.want.err == nil {
+				t.Errorf("expected error: %v, got: %v", tc.want.err, err)
+			} else if err.Error() != tc.want.err.Error() {
+				t.Errorf("expected error: %v, got: %v", tc.want.err, err)
+			}
+		})
+	}
+
+}
