53b10a885bdf4de64e780b703149ba7fd3784f52
Pedro Medina <pedromedina190@gmail.com>
2025-02-21 14:18:29 -0300

    fix code for ci

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -16,7 +16,7 @@ func GetAPIKey(headers http.Header) (string, error) {
 	}
 	splitAuth := strings.Split(authHeader, " ")
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
-		return "", errors.New("malformed authorization headrer")
+		return "", errors.New("malformed authorization header")
 	}
 
 	return splitAuth[1], nil
