d362713ad23a6cecf0d462ec889eb2bcf4de3915
Raymund Cueva <racueva@globe.com.ph>
2025-02-22 21:55:43 +0800

    updated ci.yml

--- main.go
+++ main.go
@@ -24,10 +24,11 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
-func main(){ 
+func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)	}
+		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
+	}
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -94,4 +95,9 @@ func main(){
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func unused() {
+    // this function does nothing
+    // and is called nowhere
 }
