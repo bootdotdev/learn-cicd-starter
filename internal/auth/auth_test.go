e7ae8001462d840b3e8a53fe5056a79ecaae9ea8
VannestBun <114324735+VannestBun@users.noreply.github.com>
2025-02-26 19:10:38 +1100

    add formating style test

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -5,24 +5,24 @@ import (
 	"testing"
 )
 
-func TestGetAPIKey (t *testing.T) {
+func TestGetAPIKey(t *testing.T) {
 	testCases := []struct {
-		name string
-		headers http.Header
+		name           string
+		headers        http.Header
 		expectedAPIKey string
-		expectedError error
+		expectedError  error
 	}{
 		{
-			name:            "Valid API Key",
-			headers:         http.Header{"Authorization": {"ApiKey valid-api-key"}},
-			expectedAPIKey:  "valid-api-key",
-			expectedError:   nil,
+			name:           "Valid API Key",
+			headers:        http.Header{"Authorization": {"ApiKey valid-api-key"}},
+			expectedAPIKey: "valid-api-key",
+			expectedError:  nil,
 		},
 		{
-			name:            "No Authorization Header",
-			headers:         http.Header{},
-			expectedAPIKey:  "",
-			expectedError:   ErrNoAuthHeaderIncluded,
+			name:           "No Authorization Header",
+			headers:        http.Header{},
+			expectedAPIKey: "",
+			expectedError:  ErrNoAuthHeaderIncluded,
 		},
 	}
 
@@ -30,14 +30,14 @@ func TestGetAPIKey (t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			apiKey, err := GetAPIKey(tc.headers)
 
-						if tc.expectedError != nil && err == nil {
-							t.Errorf("Test Case: %s - Expected error, but got none", tc.name)
-							return // Stop if error was expected but not received
-						}
-			
-						if apiKey != tc.expectedAPIKey {
-							t.Errorf("Test Case: %s - API Key - Expected '%s', got '%s'", tc.name, tc.expectedAPIKey, apiKey)
-						}
+			if tc.expectedError != nil && err == nil {
+				t.Errorf("Test Case: %s - Expected error, but got none", tc.name)
+				return // Stop if error was expected but not received
+			}
+
+			if apiKey != tc.expectedAPIKey {
+				t.Errorf("Test Case: %s - API Key - Expected '%s', got '%s'", tc.name, tc.expectedAPIKey, apiKey)
+			}
 		})
 	}
 }
