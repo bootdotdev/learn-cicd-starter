8cc86c202906fc9539c0784f271dc7909ccf4aa7
semifitcatholic <vaughannr@gmail.com>
2025-02-27 10:25:11 -0600

    adding temporary test error

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,52 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	type args struct {
+		headers http.Header
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "valid api key",
+			args: args{
+				headers: http.Header{
+					// "Authorization": {"ApiKey 123"},
+					"Authorization": {"Bearer 123"},
+				},
+			},
+			want:    "123",
+			wantErr: false,
+		},
+		{
+			name: "invalid api key",
+			args: args{
+				headers: http.Header{
+					"Authorization": {"Bearer 123"},
+				},
+			},
+			want:    "",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetAPIKey(tt.args.headers)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetAPIKey() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("GetAPIKey() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
