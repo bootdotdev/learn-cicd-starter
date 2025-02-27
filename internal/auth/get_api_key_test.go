000d5db0a63b079da90471d9320aff2fe9333ecc
Av26qcDL <kyle.jacunski@yahoo.com>
2025-02-26 19:03:01 -0500

    Fix test: restore expected value to xxxxxx

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -34,7 +34,7 @@ func TestGetAPIKey(t *testing.T) {
 		{
 			key:       "Authorization",
 			value:     "ApiKey xxxxxx",
-			expect:    "yyyy",
+			expect:    "xxxxxx",
 			expectErr: "not expecting an error",
 		},
 	}
