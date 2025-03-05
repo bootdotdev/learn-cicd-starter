d3bb1aef7c1403a7efec08908e49c42c6a4c4e17
LeoMarius <mariusleo64@gmail.com>
2025-03-05 10:32:48 +0100

    verification du test

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -19,6 +19,6 @@ func GetAPIKey(headers http.Header) (string, error) {
 		return "", errors.New("malformed authorization header")
 	}
 
-	return "", nil
-	// return splitAuth[1], nil
+	// return "", nil
+	return splitAuth[1], nil
 }
