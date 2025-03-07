bc413c6c8890358a17714f1ab31cde7d188289ac
mambo-dev <mambo.michael.22@gmail.com>
2025-03-07 10:21:58 +0300

    security fixes

--- json.go
+++ json.go
@@ -27,5 +27,10 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	_, err = w.Write(dat)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to send payload")
+		return
+	}
 }
