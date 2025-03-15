a5b7ed7e96c3908fed955465b159ab3b4c39101b
TDEN93 <denney.tray@gmail.com>
2025-03-15 08:09:48 -0500

    fixed auth.go

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -15,7 +15,7 @@ func GetAPIKey(headers http.Header) (string, error) {
 		return "", ErrNoAuthHeaderIncluded
 	}
 	splitAuth := strings.Split(authHeader, " ")
-	if len(splitAuth) < 2 || splitAuth[0] != "Bearer" {
+	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
 		return "", errors.New("malformed authorization header")
 	}
 
