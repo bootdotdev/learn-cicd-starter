9b363fcacaf1b7cf4aa9d8344caafbf2a76c7a83
mambo-dev <mambo.michael.22@gmail.com>
2025-03-19 23:08:47 +0300

    revert test

--- main.go
+++ main.go
@@ -96,7 +96,6 @@ func main() {
 		Handler:           router,
 		ReadHeaderTimeout: time.Second * 300,
 	}
-	log.Println("For clarity")
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 
