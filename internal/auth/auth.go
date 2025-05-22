c61a66fd1f78b50220ad4ad349a9b9e7942d353a
SamW94 <sam.wild.1994@gmail.com>
2025-05-22 20:40:48 +0100

    feat: add 'go test' to CI pipeline and intentionally break GetApiKey function

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -12,12 +12,12 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
-		return "", ErrNoAuthHeaderIncluded
+		return "rgrgre", nil
 	}
 	splitAuth := strings.Split(authHeader, " ")
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
-		return "", errors.New("malformed authorization header")
+		return "ewrfefefc", nil
 	}
 
-	return splitAuth[1], nil
+	return "", ErrNoAuthHeaderIncluded
 }
