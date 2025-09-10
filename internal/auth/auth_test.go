2b15c8dc5581231a2dc510b30c85e889fc6ca409
Harsha Vardhan Mirthinti <mhv2408@gmail.com>
2025-09-10 10:19:36 -0700

    edit: updated test still to fail

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -19,7 +19,7 @@ func TestGetAPIKey(t *testing.T) {
 		},
 		{
 			key:       "Authorization",
-			expectErr: "malformed authorization header",
+			expectErr: "no authorization header",
 		},
 		{
 			key:       "Authorization",
