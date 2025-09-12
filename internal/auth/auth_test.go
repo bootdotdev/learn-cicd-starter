563519f7fcefe7064210be23c0a58c36e643e726
sean <seandisero@gmail.com>
2025-09-12 12:08:56 -0700

    adding linting with staticcheck

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -13,21 +13,25 @@ func TestGetAPIKey(t *testing.T) {
 	key, err := GetAPIKey(testHeader)
 	assert.NotNil(t, err)
 	assert.Equal(t, err.Error(), "no authorization header included", "expected error message to be equal")
+	assert.Equal(t, key, "", "key should be ''")
 
 	testHeader.Set("Authorization", "")
 	key, err = GetAPIKey(testHeader)
 	assert.NotNil(t, err)
 	assert.Equal(t, err.Error(), "no authorization header included", "expected error message should be equal")
+	assert.Equal(t, key, "", "key should be ''")
 
 	testHeader.Set("Authorization", "Bearer thisismyapikey")
 	key, err = GetAPIKey(testHeader)
 	assert.NotNil(t, err)
 	assert.Equal(t, err.Error(), "malformed authorization header", "expected error message should be equal")
+	assert.Equal(t, key, "", "key should be ''")
 
 	testHeader.Set("Authorization", "ApiKeythisismyapikey")
 	key, err = GetAPIKey(testHeader)
 	assert.NotNil(t, err)
 	assert.Equal(t, err.Error(), "malformed authorization header", "expected error message should be equal")
+	assert.Equal(t, key, "", "key should be ''")
 
 	testHeader.Set("Authorization", "ApiKey thisismyapikey")
 	key, err = GetAPIKey(testHeader)
