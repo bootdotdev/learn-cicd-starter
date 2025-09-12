966550c7d2d95c8ffe44a1542136cb1b1405a7cf
sean <seandisero@gmail.com>
2025-09-12 10:54:39 -0700

    commiting tests for auth package

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,37 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	testHeader := http.Header{}
+
+	key, err := GetAPIKey(testHeader)
+	assert.Nil(t, err)
+	assert.Equal(t, err.Error(), "no authorization header included", "expected error message to be equal")
+
+	testHeader.Set("Authorization", "")
+	key, err = GetAPIKey(testHeader)
+	assert.NotNil(t, err)
+	assert.Equal(t, err.Error(), "no authorization header included", "expected error message should be equal")
+
+	testHeader.Set("Authorization", "Bearer thisismyapikey")
+	key, err = GetAPIKey(testHeader)
+	assert.NotNil(t, err)
+	assert.Equal(t, err.Error(), "malformed authorization header", "expected error message should be equal")
+
+	testHeader.Set("Authorization", "ApiKeythisismyapikey")
+	key, err = GetAPIKey(testHeader)
+	assert.NotNil(t, err)
+	assert.Equal(t, err.Error(), "malformed authorization header", "expected error message should be equal")
+
+	testHeader.Set("Authorization", "ApiKey thisismyapikey")
+	key, err = GetAPIKey(testHeader)
+	assert.Nil(t, err)
+	assert.Equal(t, key, "thisismyapikey", "they should be equal")
+
+}
