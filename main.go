2f68bc0669c0ba5c7aaf046370bf3e99f4571f40
ivan bonifazi <ivanbonifazi@gmail.com>
2025-10-31 14:03:51 +0100

    Removed unused Function

--- main.go
+++ main.go
@@ -1,42 +1,98 @@
-name: ci
+package main
 
-on:
-  pull_request:
-    branches: [main]
+import (
+	"database/sql"
+	"embed"
+	"io"
+	"log"
+	"net/http"
+	"os"
 
-jobs:
-  tests:
-    name: Tests
-    runs-on: ubuntu-latest
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 
-    steps:
-      - name: Check out code
-        uses: actions/checkout@v4
+	"github.com/bootdotdev/learn-cicd-starter/internal/database"
 
-      - name: Set up Go
-        uses: actions/setup-go@v5
-        with:
-          go-version: "1.22"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
+)
 
-      - name: Run tests
-        run: go test ./... --cover
+type apiConfig struct {
+	DB *database.Queries
+}
 
-  style:
-    name: Style
-    runs-on: ubuntu-latest
+//go:embed static/*
+var staticFiles embed.FS
 
-    steps:
-      - name: Check out code
-        uses: actions/checkout@v4
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
+	}
 
-      - name: Set up Go
-        uses: actions/setup-go@v5
-        with:
-          go-version: "1.22"
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
 
-      - name: Check formatting
-        run: test -z $(go fmt ./...)
+	apiCfg := apiConfig{}
 
-      - name: Run staticcheck
-        run: staticcheck ./...
+	// https://github.com/libsql/libsql-client-go/#open-a-connection-to-sqld
+	// libsql://[your-database].turso.io?authToken=[your-auth-token]
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Println("DATABASE_URL environment variable is not set")
+		log.Println("Running without CRUD endpoints")
+	} else {
+		db, err := sql.Open("libsql", dbURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		dbQueries := database.New(db)
+		apiCfg.DB = dbQueries
+		log.Println("Connected to database!")
+	}
 
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		f, err := staticFiles.Open("static/index.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer f.Close()
+		if _, err := io.Copy(w, f); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	v1Router := chi.NewRouter()
+
+	if apiCfg.DB != nil {
+		v1Router.Post("/users", apiCfg.handlerUsersCreate)
+		v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerUsersGet))
+		v1Router.Get("/notes", apiCfg.middlewareAuth(apiCfg.handlerNotesGet))
+		v1Router.Post("/notes", apiCfg.middlewareAuth(apiCfg.handlerNotesCreate))
+	}
+
+	v1Router.Get("/healthz", handlerReadiness)
+
+	router.Mount("/v1", v1Router)
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: router,
+	}
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
+}
