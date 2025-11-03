0d5e306eaade3bfcbfa617384a6f59a89b8e1307
ivan bonifazi <ivanbonifazi@gmail.com>
2025-11-03 12:55:44 +0100

    Format code with go fmt

--- json.go
+++ json.go
@@ -30,7 +30,7 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-        if _, err := w.Write(dat); err != nil {
-            log.Printf("Error writing response: %v", err)
-        }
+	if _, err := w.Write(dat); err != nil {
+		log.Printf("Error writing response: %v", err)
+	}
 }
