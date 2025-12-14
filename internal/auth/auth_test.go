d2878d255d9cc2199a5aa4897e356e6baf6af5cb
Yash Pal Singh <ypsingh@Yashs-Mac-mini.local>
2025-12-15 01:44:30 +0530

    unit tests fixed.

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -17,7 +17,7 @@ func TestGetAPIKey(t *testing.T) {
 		{
 			name:          "No Authorization Header",
 			headers:       http.Header{}, // Empty headers
-			expectedKey:   "asdf",
+			expectedKey:   "",
 			expectedError: ErrNoAuthHeaderIncluded,
 		},
 		{
