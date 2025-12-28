b7bb931633f0727da4c008a0d96d7c74b55433d9
arch563 <archiepullan@gmail.com>
2025-12-28 23:49:48 +0000

    added test

---
+++ internal/auth/test_auth.go
@@ -0,0 +1,16 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	header := http.Header{}
+
+	header.Set("Authorization", "ApiKey sometext")
+	res, _ := GetAPIKey(header)
+	if res != "sometext" {
+		t.Errorf("expected sometext, got %s", res)
+	}
+}
