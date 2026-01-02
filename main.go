c2e751be0fe6fe6d8e17dd3b12df5a51c6d96eea
yaroslav <y.kr90.ua@gmail.com>
2026-01-02 15:08:54 +0100

    Break logic in main.go to verify CI failure

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
 	}
 
-	port := os.Getenv("PORT")
+	port := 1234("PORT")
 	if port == "" {
 		log.Fatal("PORT environment variable is not set")
 	}
