b1c77bdb27201fa3bf3f8a2c5af89202be53bb58
thomas-reed <thomas.h.reed@gmail.com>
2026-01-31 17:11:58 -0500

    fix formatting issue

--- main.go
+++ main.go
@@ -90,8 +90,8 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:        ":" + port,
+		Handler:     router,
 		ReadTimeout: 10 * time.Second,
 	}
 
