cc052b82598e7913757af72afdd1fa1687409885
ryan nemeth <ryanthomasnemeth@gmail.com>
2026-01-31 11:21:18 -0500

    break code on purpose

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,20 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	header := http.Header{}
+	header.Add("Authorization", "ApiKey m-secret")
+
+	apiKey, err := GetAPIKey(header)
+	if err != nil {
+		t.Fatal("expected no error, got:", err)
+	}
+
+	if apiKey != "my-secret" {
+		t.Fatalf("expected api key to be: my-secret, got: %v", apiKey)
+	}
+}
