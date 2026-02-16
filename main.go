c8a5721e3a2f7cf116df768d4ab56743ccce19f6
razvan <razvan.boita@katty-fashion.ro>
2026-02-16 19:06:34 +0200

    A

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 	"log"
 	"net/http"
 	"os"
-
+	"time"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
-
+	"strconv"
 	"github.com/bootdotdev/learn-cicd-starter/internal/database"
 
 	_ "github.com/tursodatabase/libsql-client-go/libsql"
@@ -90,10 +90,16 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+	    Addr:              ":" + port,
+	    Handler:           router,
+	    ReadHeaderTimeout: 5 * time.Second,
+	    ReadTimeout:       10 * time.Second,
+	    WriteTimeout:      10 * time.Second,
+	    IdleTimeout:       60 * time.Second,
 	}
-
+	if _, err := strconv.Atoi(port); err != nil {
+	    log.Fatal("PORT must be numeric")
+	}
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
