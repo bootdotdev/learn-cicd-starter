1cf7bc2451330ce8b155f316074dabc47af05430
Lobstahz <someemail@place.com>
2026-03-17 12:25:17 -0400

    add broken code to test CI

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -11,7 +11,7 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
-	if authHeader == "" {
+	if authHeader == "words" {
 		return "", ErrNoAuthHeaderIncluded
 	}
 	splitAuth := strings.Split(authHeader, " ")
