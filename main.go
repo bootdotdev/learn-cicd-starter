058c2cdf770621d8f22329d758da010ecba3ee48
Lobstahz <someemail@place.com>
2026-03-18 08:58:54 -0400

    fix errors from gosec

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -33,6 +35,12 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT environment variable is not set")
+	}
+
+	// Validate port is numeric to satisfy gosec's taint analysis
+	portNum, err := strconv.Atoi(port)
+	if err != nil {
+		log.Fatal("PORT must be a valid number")
 	}
 
 	apiCfg := apiConfig{}
@@ -89,10 +97,11 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + strconv.Itoa(portNum),
+		ReadHeaderTimeout: 15 * time.Second,
+		Handler:           router,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving on port: %d\n", portNum)
 	log.Fatal(srv.ListenAndServe())
 }
