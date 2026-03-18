b5574f7991b5078c2afc7418324cb8aab2503ac2
Athens Fitzcheung <athens.fitzcheung@gmail.com>
2026-03-18 14:50:14 -0400

    fixed auth function from deliberate break

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -11,7 +11,7 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
 	// authHeader := headers.Get("Authorization")
-	authHeader := headers.Get("Authorisation")
+	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
