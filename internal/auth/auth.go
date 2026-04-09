74c99f33a23e60836c04d4b36fd2e295a838459b
lowest1337 <e1326220@student.tuwien.ac.at>
2026-04-09 08:51:23 +0200

    test now fixed, workflow should run through

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -14,7 +14,6 @@ func GetAPIKey(headers http.Header) (string, error) {
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
-	return 1
 	splitAuth := strings.Split(authHeader, " ")
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
 		return "", errors.New("malformed authorization header")
