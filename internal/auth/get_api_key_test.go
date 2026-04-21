f05a9ff9fef60263681a84c32f325c5efa56ee3a
nonso7 <felixmedia78@gmail.com>
2026-04-21 16:21:50 +0100

    undoing what boot said i should do

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -24,7 +24,7 @@ func TestGetAPIKey(t *testing.T) {
 			headers: http.Header{
 				"Authorization": []string{"ApiKey my-secret-key"},
 			},
-			expectedKey: "wrong-key",
+			expectedKey: "my-secret-key",
 			expectedErr: nil,
 		},
 		{
