1fe50fac1a673d3f2f374f3cc51dd833fe2bc56b
Brendan <brendan-scott@live.com.au>
2026-04-21 13:24:47 +1000

    gosec passes on low?

--- json.go
+++ json.go
@@ -32,7 +32,6 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.WriteHeader(code)
 	_, err = w.Write(dat)
 	if err != nil {
-		w.WriteHeader(500)
-		return
+		log.Printf("Critical error writing response: %s", err)
 	}
 }
