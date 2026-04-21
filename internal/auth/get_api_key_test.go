f20d63584dd601378aa4d805408301d54e6a708d
nonso7 <felixmedia78@gmail.com>
2026-04-21 16:24:34 +0100

    what is going on wrongly

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -24,7 +24,7 @@ func TestGetAPIKey(t *testing.T) {
 			headers: http.Header{
 				"Authorization": []string{"ApiKey my-secret-key"},
 			},
-			expectedKey: "my-secret-key",
+			expectedKey: "wrong-key",
 			expectedErr: nil,
 		},
 		{
