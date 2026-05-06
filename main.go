50f07ef05bf716f538324a9377e88b94ac040bb2
Saud Al-Haddad <saudasalhaddad@gmail.com>
2026-05-06 20:15:00 +0300

    delete the unused function

--- main.go
+++ main.go
@@ -24,11 +24,6 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
-func unused() {
-	// this function does nothing
-	// and is called nowhere
-}
-
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
