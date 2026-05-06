617a96ceb6c48a85b2c9e11ce80858992e59dfad
Arafat Hasan <nayemarafat786@gmail.com>
2026-05-06 17:27:47 +0600

    BREAK: Change auth scheme to Bearer to test CI failure

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -14,8 +14,9 @@ func GetAPIKey(headers http.Header) (string, error) {
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
-	splitAuth := strings.Split(authHeader, " ")
-	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
+
+	splitAuth := strings.Fields(authHeader)
+	if len(splitAuth) < 2 || !strings.EqualFold(splitAuth[0], "Bearer") {
 		return "", errors.New("malformed authorization header")
 	}
 
