d615e3981c92b8d0ae3f9adeb63f82fff8465a88
Matthew <matthewridgway.deximo0820@gmail.com>
2026-05-09 17:14:15 +0800

    fix: formatting issue

--- main.go
+++ main.go
@@ -90,16 +90,16 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
 		ReadHeaderTimeout: 5 * time.Second,
-    		ReadTimeout:       10 * time.Second,
-    		WriteTimeout:      10 * time.Second,
-    		IdleTimeout:       120 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       120 * time.Second,
 	}
-	
+
 	if err != nil {
-    		log.Fatal("invalid port")
+		log.Fatal("invalid port")
 	}
 	log.Fatal(srv.ListenAndServe())
 }
