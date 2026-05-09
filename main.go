9c9228d0c812b0a4971d2594ec3c32d02c61d168
Matthew <matthewridgway.deximo0820@gmail.com>
2026-05-09 17:11:34 +0800

    fix security issues

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -91,8 +92,14 @@ func main() {
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: router,
+		ReadHeaderTimeout: 5 * time.Second,
+    		ReadTimeout:       10 * time.Second,
+    		WriteTimeout:      10 * time.Second,
+    		IdleTimeout:       120 * time.Second,
 	}
-
-	log.Printf("Serving on port: %s\n", port)
+	
+	if err != nil {
+    		log.Fatal("invalid port")
+	}
 	log.Fatal(srv.ListenAndServe())
 }
