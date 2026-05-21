6d5e5d568c66ac385161e20e7937c0b2046348fc
AlienAscension <linus.breitenberger@gmail.com>
2026-05-21 11:50:04 +0200

    add broken code to test

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -12,12 +12,12 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
-		return "bruh", ErrNoAuthHeaderIncluded
+		return "", ErrNoAuthHeaderIncluded
 	}
 	splitAuth := strings.Split(authHeader, " ")
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
 		return "", errors.New("malformed authorization header")
 	}
 
-	return splitAuth[1], nil
+	return string("bruh"), nil
 }
