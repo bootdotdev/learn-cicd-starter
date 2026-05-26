c19b5a0cacfbaae0837f0c4ce69422b595c7c45f
ulfurloyd <eklavyasood@gmail.com>
2026-05-26 22:34:23 +0530

    feat: break function

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -11,7 +11,7 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
-	if authHeader == "" {
+	if authHeader != "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
 	splitAuth := strings.Split(authHeader, " ")
