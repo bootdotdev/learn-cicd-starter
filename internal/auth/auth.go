fabfc662ef5c02d0d4d88c6e2f7d55818464f71f
mrbaker1917 <mrbaker1917@gmail.com>
2026-06-05 10:41:46 -0700

    adds test but with mistake

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -11,11 +11,12 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
+	print(authHeader)
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
 	splitAuth := strings.Split(authHeader, " ")
-	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
+	if len(splitAuth) < 3 || splitAuth[0] != "ApiKey" {
 		return "", errors.New("malformed authorization header")
 	}
 
