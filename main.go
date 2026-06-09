860c2d10a0464265623309c09ec5a3b002ab5410
Mbonnic-psx <matthewbonnichsen@gmail.com>
2026-06-08 19:15:28 -0500

    Fixing Code #2

--- main.go
+++ main.go
@@ -100,6 +100,6 @@ func main() {
 		ReadHeaderTimeout: 10 * time.Second,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving on port: %d\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
