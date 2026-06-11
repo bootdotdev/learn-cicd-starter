be992e4054245ca7d2a613083b4ea86707274377
PlinyTheYounger0 <PlinyTheYounger0@proton.me>
2026-06-11 00:03:36 -0700

    This should'nt fail CI

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -37,12 +37,6 @@ func TestGetAPIKey(t *testing.T) {
 			expect:    "xxxxxx",
 			expectErr: "not expecting an error",
 		},
-		{
-			key:       "Authorization",
-			value:     "ApiKey xxxxxx",
-			expect:    "ThisShouldFail",
-			expectErr: "not expecting an error",
-		},
 	}
 
 	for i, test := range tests {
