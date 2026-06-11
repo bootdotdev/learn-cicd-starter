e8d8449dee3c2a7fee23cd9f126bd0afbd99d664
PlinyTheYounger0 <PlinyTheYounger0@proton.me>
2026-06-11 00:36:19 -0700

    Added automatic go formatting to CI

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
-func main(){
+func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
