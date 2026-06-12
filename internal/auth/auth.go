1927a728e8d0cd5cd6671f2eaff6e4edc38d86de
yassineafaila <yassineafaila55@gmail.com>
2026-06-12 16:53:42 +0100

    Feat: remove the tests

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -11,7 +11,6 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
-	x := "yassine"
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
