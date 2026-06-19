a990edd0173d0d53addce2c600660ea7cb935369
azizu06 <ab725492@ucf.edu>
2026-06-19 11:07:29 -0400

    Add tests for authorizations.

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,30 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	headers := http.Header{}
+	headers.Set("Authorization", "ApiKey 123456")
+	got, err := GetAPIKey(headers)
+	if err != nil {
+		t.Fatalf("error")
+	}
+	want := "123456"
+	if got != want {
+		t.Fatalf("wrong key")
+	}
+}
+
+func TestGetAPIKeyNoHeader(t *testing.T) {
+	headers := http.Header{}
+	got, err := GetAPIKey(headers)
+	if err != ErrNoAuthHeaderIncluded {
+		t.Fatalf("error")
+	}
+	if got != "" {
+		t.Fatalf("wrong key")
+	}
+}
