adc069be66d175c8c77ce6ea339dbf6cf19ce7ae
Srikumar6529 <sridivijendrakumar@gmail.com>
2026-06-22 20:59:13 -0500

    created two jobs in ci to run in parallel

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -1,14 +1,14 @@
 package auth
 
 import (
-	"testing"
-	"reflect"
 	"net/http"
+	"reflect"
+	"testing"
 )
 
 func TestGetAPIKey(t *testing.T) {
 	got, _ := GetAPIKey(http.Header{
-		"Authorization": []string{"ApiKey test"},})
+		"Authorization": []string{"ApiKey test"}})
 	want := "test"
 	if !reflect.DeepEqual(got, want) {
 		t.Fatalf("got %v, want %v", got, want)
