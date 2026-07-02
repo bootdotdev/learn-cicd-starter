dc2eefbacc8dfee6ad42844c1eed300f26f9fd68
Gaston Coria <gaston.coria@uala.com.ar>
2026-07-01 22:47:43 -0300

    add sample test

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,20 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	header := http.Header{}
+	header.Set("Authorization", "ApiKey test")
+
+	apiKey, err := GetAPIKey(header)
+	if err != nil {
+		t.Fatalf("Failed to get API key: %v", err)
+	}
+	if apiKey != "test" {
+		t.Fatalf("Expected API key to be 'test', got '%s'", apiKey)
+	}
+
+}
