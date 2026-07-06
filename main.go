217fd3d88ecb3cb9ff9c9fd1ae941fa49a1bf57e
habtse <habtetsegaye19@gmail.com>
2026-07-06 16:13:37 +0300

    security issue is fixed

--- main.go
+++ main.go
@@ -88,10 +88,10 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: 10 * time.Second,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
