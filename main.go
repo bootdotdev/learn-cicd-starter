ec943bc2b95996029f0f381c9f7052fd8a85b5be
habtse <habtetsegaye19@gmail.com>
2026-07-06 15:50:58 +0300

    ci file is added

--- main.go
+++ main.go
@@ -23,9 +23,10 @@ type apiConfig struct {
 
 //go:embed static/*
 var staticFiles embed.FS
+
 func unused() {
-    // this function does nothing
-    // and is called nowhere
+	// this function does nothing
+	// and is called nowhere
 }
 func main() {
 	err := godotenv.Load(".env")
