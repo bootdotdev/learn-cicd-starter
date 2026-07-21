f521724f6e4cc490d8d754096717c13c8b5e1f0b
Rivaldi Putra <36479850+vadhe@users.noreply.github.com>
2026-07-21 11:16:45 +0800

    fix gosec

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -33,6 +34,10 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT environment variable is not set")
+	}
+	portInt, err := strconv.Atoi(port)
+	if err != nil {
+    log.Fatal("Format port tidak valid")
 	}
 
 	apiCfg := apiConfig{}
@@ -91,8 +96,9 @@ func main() {
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: router,
+		ReadTimeout: 60 * 60 * 60,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving on port: %d\n", portInt)
 	log.Fatal(srv.ListenAndServe())
 }
