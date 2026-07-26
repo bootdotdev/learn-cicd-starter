0f0b8da0855c48d8f05a588eff170cac2c4cc80a
Tom Goodison <tomgoodo@protonmail.com>
2026-07-26 16:15:41 +1000

    refactor to fix gosec errors

--- main.go
+++ main.go
@@ -89,10 +89,11 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: 5,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving on port: %s\n", port) // #nosec G706
 	log.Fatal(srv.ListenAndServe())
 }
