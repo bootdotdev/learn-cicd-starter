41d97e8fc29f582a5fc265fdbd6f0f7f2f7d286b
Ron-27 <ronmaor@sas.upenn.edu>
2026-07-26 23:34:01 -0700

    final

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -3,6 +3,5 @@ package auth
 import "testing"
 
 func TestGetAPIKey(t *testing.T) {
-	// This will immediately fail the test
-	t.Fatal("Intentional failure to test CI pipeline!")
+	// Empty test passes automatically
 }
