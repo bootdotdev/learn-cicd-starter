74bfc4a8b7d9497ad2a996955e514df79e1a9a1f
jtpoplin <jason.poplin@gmail.com>
2026-07-27 08:53:53 +0200

    Add style job to ci.yml file

--- main.go
+++ main.go
@@ -90,8 +90,7 @@ func main() {
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
 		Addr:    ":" + port,
-		Handler: router,
-	}
+		Handler: router}
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
