c7281e92c495bf1dab852d627ccd4f4f44647c13
Aycoi <a.amoudi95@gmail.com>
2026-08-04 23:13:28 +0300

    add staticcheck to workflow

--- main.go
+++ main.go
@@ -95,4 +95,11 @@ func main() {
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
+
+}
+
+
+func unused() {
+	// this function does nothing
+	// and is called nowhere
 }
