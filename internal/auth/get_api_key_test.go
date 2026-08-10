8250821abbe0ef72f35ada446840ae967d68df98
shaunfg <shaunfendigan@gmail.com>
2026-08-10 17:03:04 +0800

    temporary break

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,20 @@
+package auth
+
+import (
+	"testing"
+	"net/http"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	headers := http.Header{}
+	headers.Set("Authorization", "ApiKey abc123")
+
+	got, err := GetAPIKey(headers)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	want := "abc123"
+	if want != got {
+		t.Fatalf("expected: %v, got: %v", want, got)
+	}
+}
