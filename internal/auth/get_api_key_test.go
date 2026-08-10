cfd0d2664be2cf549c177d0accb6af81aaf17a26
MarkDavidSanders <markdavidsanders@gmail.com>
2026-08-10 14:38:33 -0400

    test test

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,35 @@
+package auth
+
+import (
+	"net/http/httptest"
+	"testing"
+)
+
+var testReq = httptest.NewRequest("GET", "/api", nil)
+
+func TestGetAPIKeyGoodAuth(t *testing.T) {
+	testReq.Header.Set("Authorization", "ApiKey42069")
+	want := "42069"
+	got, err := GetAPIKey(testReq.Header)
+	if err != nil {
+		t.Errorf("GetAPIKey() returned an error: %v", err)
+	}
+	if got != want {
+		t.Errorf("GetAPIKey() = %v, want %v", got, want)
+	}
+}
+
+func TestGetAPIKeyBadAuth(t *testing.T) {
+	testReq.Header.Set("Authorization", "Bearer 42069")
+	got, err := GetAPIKey(testReq.Header)
+	if err == nil {
+		t.Errorf("GetAPIKey() did not return an error; got %v", got)
+	}
+}
+
+func TestGetAPIKeyNoAuth(t *testing.T) {
+	got, err := GetAPIKey(testReq.Header)
+	if err == nil {
+		t.Errorf("GetAPIKey() did not flag missing header as expected; got %v", got)
+	}
+}
