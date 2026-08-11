84cd439a24fdb3175b30018617cf067240190b72
Grimfilerino <me@grimfilerino.com>
2026-08-11 15:27:57 +0200

    Added tests

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,29 @@
+package auth_test
+
+import (
+	"net/http"
+    "testing"
+
+	"github.com/bootdotdev/learn-cicd-starter/internal/auth"
+)
+
+
+func TestGetAPIKeyNoHeader(t *testing.T) {
+	_, err := auth.GetAPIKey(http.Header{})
+	
+	if err == nil {
+		t.Errorf("Get API Key worked despite no headers")	
+	}
+
+}
+
+
+func TestGetAPIKey(t *testing.T) {
+	_, err := auth.GetAPIKey(http.Header{
+		"Authorization" : []string{"ApiKey aslhdklash23908743kjshdf"}, 
+	})
+
+	if err == nil {
+		t.Errorf("It should work")
+	}
+}
