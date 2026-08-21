9fbe05ee4c3854fe9b3ab6099ff6488db477c1fa
Adrian Topolski <adrian.topolski@headbox.com>
2026-08-21 14:49:13 +0100

    Tests

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -8,6 +8,8 @@ import (
 
 var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
+var ErrMalformedAuthHeader = errors.New("malformed authorization header")
+
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
@@ -16,7 +18,7 @@ func GetAPIKey(headers http.Header) (string, error) {
 	}
 	splitAuth := strings.Split(authHeader, " ")
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
-		return "", errors.New("malformed authorization header")
+		return "", ErrMalformedAuthHeader
 	}
 
 	return splitAuth[1], nil
